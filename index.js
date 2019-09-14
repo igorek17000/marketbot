895b46a68d0d1db257d9d2b3527e69140635f33e
dstlmike <dstl_mike1@hotmail.com>
2019-09-14 15:52:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ server = http.createServer(function (req, res) {
   });
 });
 
-port = Number(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+port = Number(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
 ip = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
 
 server.listen(port, ip);
