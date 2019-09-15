4b4ffa28a6c08651f51f0da4181988f54aadd1d4
dstlmike <dstl_mike1@hotmail.com>
2019-09-15 13:37:36 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -1,25 +1,21 @@
 //var mongoDB     = require('mongodb').MongoClient;
 
-var connection_string = 'mongodb://0.0.0.0:27017/sampledb';
-
-//if(process.env.MONGODB_PASSWORD){
-//  connection_string = 'mongodb://' + process.env.MONGODB_USER + ":" +
-//  process.env.MONGODB_PASSWORD + "@" +
-//  process.env.MONGODB_SERVICE_HOST + ':' +
-//  process.env.MONGODB_SERVICE_PORT + '/' +
-//  process.env.MONGODB_DATABASE;
-//}
-
-
-var MongoClient = require('mongodb').MongoClient;
-
-var uri = "mongodb://User6T6:308boonave@sampledb-shard-00-00-evpdx.gcp.mongodb.net:27017,sampledb-shard-00-01-evpdx.gcp.mongodb.net:27017,sampledb-shard-00-02-evpdx.gcp.mongodb.net:27017/test?ssl=true&replicaSet=sampledb-shard-0&authSource=admin&retryWrites=true&w=majority";
+var uri = "mongodb://User6T6:<password>@sampledb-shard-00-00-evpdx.gcp.mongodb.net:27017,sampledb-shard-00-01-evpdx.gcp.mongodb.net:27017,sampledb-shard-00-02-evpdx.gcp.mongodb.net:27017/test?ssl=true&replicaSet=sampledb-shard-0&authSource=admin&retryWrites=true&w=majority";
 MongoClient.connect(uri, function(err, client) {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
 });
 
+var connection_string = 'mongodb://0.0.0.0:27017/sampledb';
+
+if(process.env.MONGODB_PASSWORD){
+  connection_string = 'mongodb://' + process.env.MONGODB_USER + ":" +
+  process.env.MONGODB_PASSWORD + "@" +
+  process.env.MONGODB_SERVICE_HOST + ':' +
+  process.env.MONGODB_SERVICE_PORT + '/' +
+  process.env.MONGODB_DATABASE;
+}
 
 function connect(callback){
   mongoDB.connect(connection_string, function(err, db) {
