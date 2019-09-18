b20c2365641261275ce618e6d2107d86a98fff86
dstlmike <dstl_mike1@hotmail.com>
2019-09-17 20:24:13 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -2,7 +2,7 @@ var mongoDB     = require('mongodb').MongoClient;
 
 //var connection_string = 'mongodb://0.0.0.0:27017/sampledb';
 
-var connection_string = 'mongodb+srv://dstlmike:308boonave@cluster0-esmha.mongodb.net/test';
+var connection_string = 'mongodb+srv://dstlmike1:308boonave@cluster0-esmha.mongodb.net/test';
 
 if(process.env.MONGODB_PASSWORD){
   connection_string = 'mongodb://' + process.env.MONGODB_USER + ":" +
