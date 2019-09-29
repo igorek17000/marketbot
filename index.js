7620fc7e3d1e74fc2fce90e82237eeadecfd4fe2
dstlmike <dstl_mike1@hotmail.com>
2019-09-29 00:39:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var http, director, bot, router, server, port, db;
 http        = require('http');
 director    = require('director');
 bot         = require('./bot.js');
-send        = require('gmail-send');
 
 router = new director.http.Router({
   '/'    : {
