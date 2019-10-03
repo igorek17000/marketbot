a06254e3b21dfc0fb506482a8e4e29cb71447fd5
dstlmike <dstl_mike1@hotmail.com>
2019-10-03 19:54:44 -0400

    email debug

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -119,41 +119,9 @@ function emailCmd(dataHash, callback) {
 
   if (regex.test(dataHash.request.text)) {
     if (dataHash.isMod) {
+var email = require('./server');
+callback(true, email);
 
-
-      require('dotenv').config();
-
-      var nodemailer = require('nodemailer');
-
-  // step 1
-
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD
-    }
-  });
-
-  //step 2
-  let mailOptions = {
-    from: 'alexdeabot@gmail.com',
-    to: 'dstl_mike1@hotmail.com',
-    subject: 'Testing and testing',
-    text: 'It worked'
-  };
-
-  //step 3
-  transporter.sendMail(mailOptions, function(err, data) {
-    if (err) {
-      console.log('error occured: ', err);
-    } else {
-      console.log('email sent!!');
-    }
-  });
-
-} else {
-  callback(true, "You are not authorized to send e-mails. ", []);
 }
   } else {
     return false;
