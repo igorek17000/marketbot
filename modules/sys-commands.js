fd342017e202916f3b06635acdd71329906cd4fb
dstlmike <dstl_mike1@hotmail.com>
2019-10-03 21:03:44 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -116,12 +116,39 @@ function aboutCmd(dataHash, callback) {
 
 function emailCmd(dataHash, callback) {
   var regex = /^\/email$/;
-var email = require('./server');
+require('dotenv').config();
+
+var nodemailer = require('nodemailer');
 
   if (regex.test(dataHash.request.text)) {
     if (dataHash.isMod) {
       
-      callback(true, email);
+      // step 1
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
+
+//step 2
+let mailOptions = {
+  from: 'alexdeabot@gmail.com',
+  to: 'dstl_mike1@hotmail.com',
+  subject: 'Testing and testing',
+  text: 'It worked'
+};
+
+//step 3
+transporter.sendMail(mailOptions, function(err, data) {
+  if (err) {
+    console.log('error occured: ', err);
+  } else {
+    console.log('email sent!!');
+  }
+});
     }
   } else {
     return false;
