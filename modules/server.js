c76363822435cdc479c9970083079607a0e601e0
dstlmike <dstl_mike1@hotmail.com>
2019-10-05 23:14:59 -0400

    Update server.js

--- modules/server.js
+++ modules/server.js
@@ -1,22 +1,22 @@
-//require('dotenv').config();
+require('dotenv').config();
 
 var nodemailer = require('nodemailer');
 //'use strict';
 // step 1
 let transporter = nodemailer.createTransport({
-  service: 'hotmail',
+  service: 'gmail',
   auth: {
-    //user: process.env.EMAIL,
-    //pass: process.env.PASSWORD
-user: 'dstl_mike1@hotmail.com',
-pass: '113Hopest!'
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+//user: 'dstl_mike1@hotmail.com',
+//pass: '113Hopest!'
   }
 });
 
 //step 2
 let mailOptions = {
-  from: 'dstl_mike1@hotmail.com',
-  to: 'alexdeabot@gmail.com',
+  from: 'alexdeabot@gmail.com',
+  to: 'dstl_mike1@hotmail.com',
   subject: 'Testing and testing',
   text: 'It worked'
 };
