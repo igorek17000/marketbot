e556e209b8e9a36577d3788574c9003070d815c5
dstlmike <dstl_mike1@hotmail.com>
2019-10-08 17:04:01 -0400

    Update server.js

--- modules/server.js
+++ modules/server.js
@@ -7,9 +7,9 @@ user: 'alexdeabot@gmail.com',
 pass: '113Hopest!',
 // Your gmail address. 
 // Not @developer.gserviceaccount.com 
-clientId: '33283131522-cq29rhd7rljomaj05rmj3akduug5ssag.apps.googleusercontent.com', 
-clientSecret: 'q4c5EwEjj55y49uI3jiOTuFf', 
-refreshToken: '1/erwx2VawfafgCpfx6N674Nzdae4klHplI9qAV4ZI5HM'
+//clientId: '33283131522-cq29rhd7rljomaj05rmj3akduug5ssag.apps.googleusercontent.com', 
+//clientSecret: 'q4c5EwEjj55y49uI3jiOTuFf', 
+//refreshToken: '1/erwx2VawfafgCpfx6N674Nzdae4klHplI9qAV4ZI5HM'
 
 } 
 //}
