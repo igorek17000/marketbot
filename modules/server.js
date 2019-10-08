6a6d94d0ab657526b9365cd72ba0d870d986a51a
dstlmike <dstl_mike1@hotmail.com>
2019-10-08 02:34:42 -0400

    Update server.js

--- modules/server.js
+++ modules/server.js
@@ -1,25 +1,25 @@
-var nodemailer = require("nodemailer"); 
+var nodemailer = require('nodemailer'); 
 var smtpTransport = nodemailer.createTransport({ 
-service: "Gmail", 
+service: 'Gmail', 
 auth: { 
 XOAuth2: { 
-user: "alexdeabot@gmail.com", 
-pass: "113Hopest!",
+user: 'alexdeabot@gmail.com', 
+pass: '113Hopest!',
 // Your gmail address. 
 // Not @developer.gserviceaccount.com 
-clientId: "33283131522-cq29rhd7rljomaj05rmj3akduug5ssag.apps.googleusercontent.com", 
-clientSecret: "q4c5EwEjj55y49uI3jiOTuFf", 
-refreshToken: "1/erwx2VawfafgCpfx6N674Nzdae4klHplI9qAV4ZI5HM" 
+clientId: '33283131522-cq29rhd7rljomaj05rmj3akduug5ssag.apps.googleusercontent.com', 
+clientSecret: 'q4c5EwEjj55y49uI3jiOTuFf', 
+refreshToken: '1/erwx2VawfafgCpfx6N674Nzdae4klHplI9qAV4ZI5HM'
 } 
 } 
 }); 
 
 var mailOptions = { 
-from: "alexdeabot@gmail.com", 
-to: "dstl_mike1@hotmail.com", 
-subject: "Hello", 
+from: 'alexdeabot@gmail.com', 
+to: 'dstl_mike1@hotmail.com', 
+subject: 'Hello', 
 generateTextFromHTML: true, 
-html: "<b>Hello world</b>" 
+html: '<b>Hello world</b>' 
 }; 
 
 smtpTransport.sendMail(mailOptions, function(error, response) { 
