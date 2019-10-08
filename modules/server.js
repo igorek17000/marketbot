ddbcde461986ea1dfccb79ea1bb0e7bbce7abe50
dstlmike <dstl_mike1@hotmail.com>
2019-10-08 16:57:57 -0400

    Update server.js

--- modules/server.js
+++ modules/server.js
@@ -1,8 +1,8 @@
 var nodemailer = require('nodemailer'); 
 var Transport = nodemailer.createTransport({ 
 service: 'Gmail', 
-//auth: { 
-XOAuth2: { 
+auth: { 
+//XOAuth2: { 
 user: 'alexdeabot@gmail.com', 
 pass: '113Hopest!',
 // Your gmail address. 
@@ -10,6 +10,7 @@ pass: '113Hopest!',
 clientId: '33283131522-cq29rhd7rljomaj05rmj3akduug5ssag.apps.googleusercontent.com', 
 clientSecret: 'q4c5EwEjj55y49uI3jiOTuFf', 
 refreshToken: '1/erwx2VawfafgCpfx6N674Nzdae4klHplI9qAV4ZI5HM'
+
 } 
 //}
 }); 
