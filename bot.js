553d64539289118d7166ffe4ebfc0c461cba87fb
dstlmike <dstl_mike1@hotmail.com>
2019-10-08 15:39:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,8 +107,9 @@ function postMessage(botResponse, attachments, botID) {
         //neat
       //} else {
         console.log('the itch to my stitch ' + res.statusCode);
-      }
-  });
+      }); //Delete <---
+}
+  //});
 
   botReq.on('error', function(err) {
     console.log('error posting message '  + JSON.stringify(err));
