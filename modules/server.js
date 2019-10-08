620310181b30c2d9f6a8cdeeac7be79eee0dae91
dstlmike <dstl_mike1@hotmail.com>
2019-10-08 01:32:12 -0400

    Update server.js

--- modules/server.js
+++ modules/server.js
@@ -3,6 +3,22 @@
 var nodemailer = require('nodemailer');
 //'use strict';
 // step 1
+
+transporter.set('oauth2_provision_cb', (user, renew, callback)=>{ 
+let accessToken = userTokens[user]; 
+if(!accessToken){ return callback(new Error('Unknown user')); 
+}else{ 
+return callback(null, accessToken); 
+} 
+});
+
+transporter.on('token', token => { 
+console.log('A new access token was generated'); 
+console.log('User: %s', token.user); 
+console.log('Access Token: %s', token.accessToken); 
+console.log('Expires: %s', new Date(token.expires)); 
+});
+
 let transporter = nodemailer.createTransport({ 
 host: 'smtp.gmail.com', 
 port: 465, 
@@ -12,6 +28,7 @@ type: 'OAuth2',
 user: 'alexdeabot@gmail.com', 
 clientId: '33283131522-cq29rhd7rljomaj05rmj3akduug5ssag.apps.googleusercontent.com', 
 clientSecret: 'q4c5EwEjj55y49uI3jiOTuFf'
+
 } 
 });
 
@@ -22,5 +39,9 @@ transporter.sendMail({
     text: 'I hope this message gets through!',
     auth: {
         user: 'alexdeabot@gmail.com'
-    }
+    refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx', 
+accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x', 
+expires: 1484314697598 } });
+
+}
 });
