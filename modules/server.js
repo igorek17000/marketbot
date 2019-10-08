7ace6cab9a00a0aca18a68259c42e2aa9110c846
dstlmike <dstl_mike1@hotmail.com>
2019-10-08 01:06:20 -0400

    Update server.js

--- modules/server.js
+++ modules/server.js
@@ -3,22 +3,15 @@
 var nodemailer = require('nodemailer');
 //'use strict';
 // step 1
-let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        type: 'OAuth2'
-    }
-});
-
-transporter.set('oauth2_provision_cb', (user, renew, callback) => {
-    let accessToken = userTokens[user];
-    if(!accessToken){
-        return callback(new Error('Unknown user'));
-    }else{
-        return callback(null, accessToken);
-    }
+let transporter = nodemailer.createTransport({ 
+host: 'smtp.gmail.com', 
+port: 465, 
+secure: true, 
+auth: { type: 'OAuth2', 
+user: 'alexdeabot@gmail.com', 
+clientId: '33283131522-cq29rhd7rljomaj05rmj3akduug5ssag.apps.googleusercontent.com', 
+clientSecret: 'q4c5EwEjj55y49uI3jiOTuFf'
+} 
 });
 
 transporter.sendMail({
