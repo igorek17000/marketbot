d9a8b7d23005b1c6ba357be617c7c985b7fe2e42
dstlmike <dstl_mike1@hotmail.com>
2019-10-09 18:29:14 -0400

    Update Close.js

--- modules/Close.js
+++ modules/Close.js
@@ -1,26 +1,33 @@
-var nodemailer = require('nodemailer'); 
-var transport = nodemailer.createTransport("SMTP", { 
-host: 'gmail', 
-Oauth2: {
+var nodemailer = require('nodemailer');
+var Transport = nodemailer.createTransport({
+service: 'gmail',
+auth: {
+//XOAuth2: {
+user: 'alexdeabot@gmail.com',
+pass: '113Hopest!',
+// Your gmail address.
+// Not @developer.gserviceaccount.com
+//clientId: '33283131522-cq29rhd7rljomaj05rmj3akduug5ssag.apps.googleusercontent.com',
+//clientSecret: 'q4c5EwEjj55y49uI3jiOTuFf',
+//refreshToken: '1/erwx2VawfafgCpfx6N674Nzdae4klHplI9qAV4ZI5HM'
 
+}
+//}
+});
 
-}); 
-
-var send_email = function (email_content) { 
-var mailOptions = { 
-from: 'me@example.com', 
-to: 'my_gmail_nickname@gmail.com', 
-subject: 'Hello!', 
-html: email_content.content 
-}; 
-
-transport.sendMail(mailOptions, function (error, info) { 
-if (error) { 
-console.log(error); 
-} else { console.log('Message sent: ' + info.message); 
-transport.close(); 
-} 
-}) 
+var mailOptions = {
+from: 'alexdeabot@gmail.com',
+to: 'dstl_mike1@hotmail.com',
+subject: 'Hello',
+generateTextFromHTML: true,
+html: '<b>Hello world</b>'
 };
 
-
+Transport.sendMail(mailOptions, function(error, response) {
+if (error) {
+console.log(error);
+} else {
+console.log(response);
+}
+Transport.close();
+});
