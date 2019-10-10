ec47fd621d417b3c6caa33546000cb59c3916cf9
dstlmike <dstl_mike1@hotmail.com>
2019-10-09 21:37:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -103,7 +103,7 @@ function postMessage(botResponse, attachments, botID) {
   console.log('sending ' + botResponse + ' to ' + botID);
 
 botReq = HTTPS.request(options, function(res) {
-      if (res.statusCode == 200) {
+      if (res.statusCode == 201) {
         //neat
       } else {
         console.log('rejecting bad status code ' + res.statusCode);
