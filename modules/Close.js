2779d878158b9e9d8ec01dfc3084d690ec45cc8e
dstlmike <dstl_mike1@hotmail.com>
2019-10-10 00:26:53 -0400

    Update Close.js

--- modules/Close.js
+++ modules/Close.js
@@ -2,14 +2,14 @@ var nodemailer = require('nodemailer');
 var Transport = nodemailer.createTransport({
 service: 'gmail',
 auth: {
-//XOAuth2: {
+XOAuth2: {
 user: 'alexdeabot@gmail.com',
-pass: '113Hopest!',
+//pass: '113Hopest!',
 // Your gmail address.
 // Not @developer.gserviceaccount.com
-//clientId: '33283131522-cq29rhd7rljomaj05rmj3akduug5ssag.apps.googleusercontent.com',
-//clientSecret: 'q4c5EwEjj55y49uI3jiOTuFf',
-//refreshToken: '1/erwx2VawfafgCpfx6N674Nzdae4klHplI9qAV4ZI5HM'
+clientId: '33283131522-cq29rhd7rljomaj05rmj3akduug5ssag.apps.googleusercontent.com',
+clientSecret: 'q4c5EwEjj55y49uI3jiOTuFf',
+refreshToken: '1/erwx2VawfafgCpfx6N674Nzdae4klHplI9qAV4ZI5HM'
 
 }
 //}
