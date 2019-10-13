18eee501012826fe7af2315e6f8aa6788248ffa4
dstlmike <dstl_mike1@hotmail.com>
2019-10-12 20:47:20 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -183,33 +183,18 @@ console.log(response);
 function nest21Cmd(dataHash, callback) {
   var regex = /^\/nest21$/;
 
-  if (regex.test(dataHash.request.text)) {
-var nodemailer = require('nodemailer');
-var Transport = nodemailer.createTransport('smtp://alexdeabot%40gmail.com:113Hopest%21@smtp-gmail.com');
+  var nest21 = require('./nest21');
+callback(error, nest21);
+} else {
+return false;
+}
 
-var mailOptions = {
-to: 'trigger@applet.ifttt.com',
-from: 'alexdeabot@gmail.com',
-subject: '#nest21',
-generateTextFromHTML: true,
-html: '<b>Nest thermostat set to 21 degrees celsius</b>'
-};
 
-Transport.sendMail(mailOptions, function(error, response) {
-//transporter.sendMail(mailOptions, function(error, info){ 
-//if(error) { 
-//res.send(400); 
-//} else { 
-//res.send(202); 
-//} 
-//});
+
 
 if (error) {
 console.log(error);
 } else {
 console.log(response);
-}
-//Transport.close();
-});
-}
+Transport.close();
 }
