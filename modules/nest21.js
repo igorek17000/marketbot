15a342510d7e7a2eba82c2ed40f438c8567a9732
dstlmike <dstl_mike1@hotmail.com>
2019-10-13 01:25:01 -0400

    Update nest21.js

--- modules/nest21.js
+++ modules/nest21.js
@@ -1,4 +1,4 @@
-var nest21 = function() {
+
 var nodemailer = require('nodemailer');
 var Transport = nodemailer.createTransport({
 
@@ -27,36 +27,3 @@ console.log(response);
 Transport.close();
 });
 }
-
-var nest20 = function() {
-var nodemailer = require('nodemailer');
-var Transport = nodemailer.createTransport({
-
-service: 'gmail',
-auth: {
-user: 'alexdeabot@gmail.com',
-pass: '113Hopest!'
-}
-});
-
-var mailOptions = {
-to: 'trigger@applet.ifttt.com',
-from: 'alexdeabot@gmail.com',
-subject: '#nest20',
-generateTextFromHTML: true,
-html: '<b>Nest thermostat set to 20 degrees celsius</b>'
-};
-
-Transport.sendMail(mailOptions, function(error, response) {
-
-if (error) {
-console.log(error);
-} else {
-console.log(response);
-}
-Transport.close();
-});
-}
-
-exports.nest20 = nest20;
-exports.nest21 = nest21;
