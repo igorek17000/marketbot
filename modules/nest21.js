763eb35c9501f00d50a14170d023cf1e265413a8
dstlmike <dstl_mike1@hotmail.com>
2019-10-13 00:00:24 -0400

    Update nest21.js

--- modules/nest21.js
+++ modules/nest21.js
@@ -1,6 +1,6 @@
-
 var nodemailer = require('nodemailer');
 var Transport = nodemailer.createTransport({
+
 service: 'gmail',
 auth: {
 user: 'alexdeabot@gmail.com',
@@ -8,7 +8,8 @@ pass: '113Hopest!'
 }
 });
 
-var mailOptions = {
+let nest21 = function() {
+let mailOptions = {
 to: 'trigger@applet.ifttt.com',
 from: 'alexdeabot@gmail.com',
 subject: '#nest21',
@@ -17,13 +18,6 @@ html: '<b>Nest thermostat set to 21 degrees celsius</b>'
 };
 
 Transport.sendMail(mailOptions, function(error, response) {
-//transporter.sendMail(mailOptions, function(error, info){ 
-//if(error) { 
-//res.send(400); 
-//} else { 
-//res.send(202); 
-//} 
-//});
 
 if (error) {
 console.log(error);
@@ -33,3 +27,28 @@ console.log(response);
 Transport.close();
 });
 }
+}
+
+let nest20 = function() {
+let mailOptions = {
+to: 'trigger@applet.ifttt.com',
+from: 'alexdeabot@gmail.com',
+subject: '#nest20',
+generateTextFromHTML: true,
+html: '<b>Nest thermostat set to 20 degrees celsius</b>'
+};
+
+Transport.sendMail(mailOptions, function(error, response) {
+
+if (error) {
+console.log(error);
+} else {
+console.log(response);
+}
+Transport.close();
+});
+}
+}
+
+exports.nest20 = nest20;
+exports.nest21 = nest21;
