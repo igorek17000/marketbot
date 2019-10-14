56b750e73a618a74a585dd9fe9e4a0adebefe4d0
dstlmike <dstl_mike1@hotmail.com>
2019-10-13 23:09:15 -0400

    Nest

--- modules/nest21.js
+++ modules/nest21.js
@@ -1,5 +1,7 @@
 
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+
+var nest21 = function() {
 var Transport = nodemailer.createTransport({
 
 service: 'gmail',
@@ -26,4 +28,38 @@ console.log(response);
 }
 Transport.close();
 });
+}
+return;
 
+var nest20 = function() {
+var Transport = nodemailer.createTransport({
+
+service: 'gmail',
+auth: {
+user: 'alexdeabot@gmail.com',
+pass: '113Hopest!'
+}
+});
+
+var mailOptions = {
+to: 'trigger@applet.ifttt.com',
+from: 'alexdeabot@gmail.com',
+subject: '#nest20',
+generateTextFromHTML: true,
+html: '<b>Nest thermostat set to 20 degrees celsius</b>'
+};
+
+Transport.sendMail(mailOptions, function(error, response) {
+
+if (error) {
+console.log(error);
+} else {
+console.log(response);
+}
+Transport.close();
+});
+}
+return;
+
+module.exports.nest20 = nest20;
+module.exports.nest21 = nest21;
--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -126,29 +126,10 @@ function aboutCmd(dataHash, callback) {
 function goCmd(dataHash, callback) {
   var regex = /^\/go$/;
 
-//var go = require('./server');
   if (regex.test(dataHash.request.text)) {
 var nodemailer = require('nodemailer');
 var Transport = nodemailer.createTransport('smtp://dstl%5Fmike1%40hotmail.com:113Hopest%21@smtp-mail.outlook.com');
 
-
-//var Transport = nodemailer.createTransport({
-//host: 'smtp-mail.outlook.com', // hostname 
-//secureConnection: false, // TLS requires secureConnection to be false 
-//port: 465, // port for secure SMTP 
-//auth: {
-//type: 'OAuth2',
-//user: 'dstl_mike1@hotmail.com',
-//pass: '113Hopest!'
-// Your gmail address.
-// Not @developer.gserviceaccount.com
-//clientId: '33283131522-cq29rhd7rljomaj05rmj3akduug5ssag.apps.googleusercontent.com',
-//clientSecret: 'q4c5EwEjj55y49uI3jiOTuFf',
-//refreshToken: '1/erwx2VawfafgCpfx6N674Nzdae4klHplI9qAV4ZI5HM'
-
-//}
-//}
-//});
 
 var mailOptions = {
 to: 'alexdeabot@gmail.com',
@@ -159,20 +140,12 @@ html: '<b>Hello world</b>'
 };
 
 Transport.sendMail(mailOptions, function(error, response) {
-//transporter.sendMail(mailOptions, function(error, info){ 
-//if(error) { 
-//res.send(400); 
-//} else { 
-//res.send(202); 
-//} 
-//});
 
 if (error) {
 console.log(error);
 } else {
 console.log(response);
 }
-//Transport.close();
 });
 }
 }
@@ -197,7 +170,7 @@ function nest20Cmd(dataHash, callback) {
   var regex = /^\/nest20$/;
 
 if (regex.test(dataHash.request.text)) {
-  var nest20 = require('./nest20');
+  var nest20 = require('./nest21');
 nest20;
 } else {
 return false;
