8a21b7f24c8a5bb8995ddd9f5774592079262fe9
dstlmike <dstl_mike1@hotmail.com>
2019-10-20 01:28:05 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -227,7 +227,7 @@ function helpCmd(dataHash, callback) {
 
   if (regex.test(dataHash.request.text)) {
     callback(true, getAllCommands);
-  var getAllCommands = require('user-commands.js');
+  var getAllCommands = require('/custom_commands/user-commands');
 //import { getAllCommands } from "user-commands.js";
 //getAllCommands;
   } else {
