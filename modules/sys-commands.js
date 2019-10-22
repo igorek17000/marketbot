24ab7ce27391465e7d1e81b375caf78f79455978
dstlmike <dstl_mike1@hotmail.com>
2019-10-22 15:23:50 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -228,8 +228,7 @@ function helpCmd(dataHash, callback) {
   if (regex.test(dataHash.request.text)) {
     if (dataHash.isMod) {
     
-var list = function () {
-  return [
+var list = [
     {cmd: "/date", desc: "Current date"},
     {cmd: "/fun", desc: "Enable commands designated as fun commands", mod: true},
     {cmd: "/nofun", desc: "Disable commands designated as fun commands", mod: true},
@@ -242,8 +241,9 @@ var list = function () {
     {cmd: "/nest21", desc: "Set Nest temperature to 21 degrees celsius"},
     {cmd: "/nest22", desc: "Set Nest temperature to 22 degrees celsius"}
   ];
+return list;
 }
-list;
+//list;
 
     //callback(true, "Command List", []);
 
