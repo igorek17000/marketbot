1b1f395ae7a07731fbb9766b2991ab546c613590
dstlmike <dstl_mike1@hotmail.com>
2019-10-26 18:15:30 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -227,29 +227,14 @@ function helpCmd(dataHash, callback) {
 
   if (regex.test(dataHash.request.text)) {
     if (dataHash.isMod) {
-    
-const findDocuments = function(db, callback) {
 
-  // Get the documents collection
+callback(true, "Command List", []);
+var db = require('./db');
+var getcmdlistdescription = require('./sys-commands');
 
-  const collection = db.collection('sampledb');
 
-  // Find some documents
-
-  collection.find({}).toArray(function(err, docs) {
-
-    assert.equal(err, null);
-
-    console.log("Found the following records");
-
-    console.log(docs)
-
-    callback(docs);
-
-  });
-
-}
-
+db.getcmdlistdescription.find({});
+return getcmdlistdescription;
 
 
     //callback(true, list);
