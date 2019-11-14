50ea5971a3d950cd150a4bfab533a88d3415f163
dstlmike <dstl_mike1@hotmail.com>
2019-11-13 23:20:01 -0500

    Update alldocs.js

--- modules/alldocs.js
+++ modules/alldocs.js
@@ -1,40 +1,13 @@
-var mongoDB     = require('mongodb').MongoClient;
-var db = require('mongodb').Db
-var db_table = 'user_triggers';
-//var connection_string = 'mongodb://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
-//var connection_string = 'mongodb://0.0.0.0:27017/test';
-var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
-
-
-//var connection_string = 'mongodb+srv://dstlmike1:308boonave@cluster0-esmha.mongodb.net/test';
-//var connection_string = 'mongodb://0.0.0.0:27017/sampledb';
-//var connection_string = 'mongodb://' + process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSWORD + "@" + process.env.MONGODB_SERVICE_HOST + ':' + process.env.MONGODB_SERVICE_PORT + '/' + process.env.MONGODB_DATABASE;
-
-
-//if(process.env.MONGODB_PASSWORD){
-  //connection_string = 'mongodb://' + process.env.MONGODB_USER + ":" +
-  //process.env.MONGODB_PASSWORD + "@" +
-  //process.env.MONGODB_SERVICE_HOST + ':' +
-  //process.env.MONGODB_SERVICE_PORT + '/' +
-  //process.env.MONGODB_DATABASE;
-//}
-
-//var connection_string = mongodb+srv://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-esmha.mongodb.net/test?retryWrites=true&w=majority
-
-if(process.env.MONGODB_PASSWORD){
-  // //connection_string = 'mongodb://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority' + process.env.MONGODB_USER + ":" +
-  connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority' + process.env.MONGODB_USER + ":" +
-  process.env.MONGODB_PASSWORD + "@" +
-  process.env.MONGODB_SERVICE_HOST + ':' +
-  process.env.MONGODB_SERVICE_PORT + '/' +
-  process.env.MONGODB_DATABASE;
-}
-
-
-  mongoDB.connect(connection_string, function(err, allDocs) {
-    if(err) throw err;
-    var allDocs = db.user_triggers.find({name : 1}, {regex: 1}, {_id: 0}).forEach(printJson);
-    return allDocs;
-      db.close();
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var query = { address: "Park Lane 38" };
+  dbo.collection("customers").find(query).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
 });
