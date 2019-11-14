4ac740c78d6649d413cd7c3897ad9d4ff525377b
dstlmike <dstl_mike1@hotmail.com>
2019-11-13 23:24:45 -0500

    Update alldocs.js

--- modules/alldocs.js
+++ modules/alldocs.js
@@ -1,11 +1,13 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+var url = "mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+//'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("mydb");
-  var query = { address: "Park Lane 38" };
-  dbo.collection("customers").find(query).toArray(function(err, result) {
+  var dbo = db.db("sampledb");
+  var query = { name: true };
+  dbo.collection("user_triggers").find(query).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
