7d147c60d9dbe59b999d34ede9ada6129282df47
dstlmike <dstl_mike1@hotmail.com>
2019-11-13 22:15:06 -0500

    Update user-commands.js

--- custom_commands/user-commands.js
+++ custom_commands/user-commands.js
@@ -19,7 +19,7 @@ db.getAllDocuments(db_table, function(res){
   });
 }
 
-function answerAllDocuments(cmd, callback) {
+function answerAllDocuments(cmd, printJson, callback) {
 db.answerAllDocuments(db_table, cmd, callback);
 }
 
