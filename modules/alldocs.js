2563db9c18b94c417aae0986e598059be6c016ce
dstlmike <dstl_mike1@hotmail.com>
2019-11-13 23:41:39 -0500

    Update alldocs.js

--- modules/alldocs.js
+++ modules/alldocs.js
@@ -7,7 +7,7 @@ MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("sampledb");
   var query = { name: true };
-  dbo.collection("user_triggers").find(query).toArray(function(err, result) {
+  db.collection(collection).find(query).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
