16e9b511c46260f7a23ab4490811cbbc1ea31976
dstlmike <dstl_mike1@hotmail.com>
2019-11-15 15:11:17 -0500

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -235,7 +235,26 @@ callback(true, docs);
 alldocs;
 //return docs;
 
-    //callback(true, list);
+    //callback(true, "Command List ", {}, []);
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+//'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("sampledb");
+  //var query = { name: "test" };
+  var query = {};
+  db.collection('user_triggers').find(query).toArray(function(err, docs) {
+    if (err) throw err;
+    console.log(docs);
+return docs;
+    //db.close();
+  });
+});
+
 
   } else {
     return false;
