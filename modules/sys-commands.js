21f7e1e40603297c5b7cdb70f5e13474b35335bb
dstlmike <dstl_mike1@hotmail.com>
2019-11-15 23:30:18 -0500

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -222,7 +222,7 @@ nest22;
 
 //-----------
 
-function helpCmd(dataHash, callback, docs) {
+function helpCmd(dataHash, callback, result) {
   var regex = /^\/help$/;
 
   if (regex.test(dataHash.request.text)) {
@@ -230,29 +230,14 @@ function helpCmd(dataHash, callback, docs) {
 var alldocs = require('./alldocs.js');
 
 
-callback(true, "Command List " {}, []);
-//var alldocs = require('./alldocs.js');
-//alldocs;
+callback(true, "Command List " + result , []);
+var alldocs = require('./alldocs.js');
+alldocs;
 //return docs;
 
     //callback(true, "Command List ", {}, []);
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
-//'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("sampledb");
-  //var query = { name: "test" };
-  var query = {};
-  db.collection('user_triggers').find(query).toArray(function(err, docs) {
-    if (err) throw err;
-    console.log(docs);
-return docs;
-    //db.close();
-  });
-});
 
 
   } else {
