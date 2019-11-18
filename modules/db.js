df9e1270f85f6dc53b303d9ae4dfa8cd9deb577e
dstlmike <dstl_mike1@hotmail.com>
2019-11-18 00:06:52 -0500

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -169,7 +169,7 @@ exports.randomDocs = function(collection, callback) {
       //var random = Math.floor(Math.random() * count);
       //cursor.skip(random);
       //cursor.limit(1);
-      cursor.sort({name:1});
+      cursor.sort({ name:1 });
       cursor.each(function(err, doc){
       //cursor.sort({'commands[cmd].name'}:1)
         if(doc != null){
