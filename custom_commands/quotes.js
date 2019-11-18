f098eb1a7af0f4005e4035be84d4b049fcc99ecc
dstlmike <dstl_mike1@hotmail.com>
2019-11-18 15:47:53 -0500

    Update quotes.js

--- custom_commands/quotes.js
+++ custom_commands/quotes.js
@@ -161,7 +161,7 @@ var dbold = ' desc: ';
 var s = '/';
 var naame = docs.name;
 //var deesc = docs.desc;
-var msg = 'cmd: /' + docs.name + ' - desc: ' + docs.description;
+var msg = 'cmd: /' + docs.name.sort(1) + ' - desc: ' + docs.description;
  
       callback(msg);
     });
