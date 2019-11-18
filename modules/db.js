ee9aaa6e3cbacb09e695dd8d67b825a506a0c373
dstlmike <dstl_mike1@hotmail.com>
2019-11-17 22:47:59 -0500

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -160,7 +160,7 @@ exports.randomDoc = function(collection, callback) {
 exports.randomDocs = function(collection, callback) {
   connect(function(db){
     var coll = db.collection('user_triggers');
-    cursor = coll.find({});
+    cursor = coll.find({}).sort( {name: 1} );
     //cursors = cursor.toArray({});
 
       coll.sort({name: 1});
