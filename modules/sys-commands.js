4ea14e5564e9506ba5fedbca955ff2023293d3c3
dstlmike <dstl_mike1@hotmail.com>
2019-11-23 21:42:21 -0500

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -33,6 +33,20 @@ exports.getCmdListDescription = function () {
   ];
 }
 
+var list = [
+    {cmd: "/date", desc: "Current date"},
+    {cmd: "/fun", desc: "Enable commands designated as fun commands", mod: true},
+    {cmd: "/nofun", desc: "Disable commands designated as fun commands", mod: true},
+    {cmd: "/id", desc: "Notifies the requester of their GroupMe ID"},
+    {cmd: "/about", desc: "Responds with a short message about the bot"},
+    {cmd: "/go", desc: "Send a test email to bot", mod: true},
+    {cmd: "/nest18", desc: "Set Nest temperature to 18 degrees celsius"},
+    {cmd: "/nest19", desc: "Set Nest temperature to 19 degrees celsius"},
+    {cmd: "/nest20", desc: "Set Nest temperature to 20 degrees celsius"},
+    {cmd: "/nest21", desc: "Set Nest temperature to 21 degrees celsius"},
+    {cmd: "/nest22", desc: "Set Nest temperature to 22 degrees celsius"}
+  ];
+
 
 function dateCmd(dataHash, callback) {
   var regex = /^\/date$/;
