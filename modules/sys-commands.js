48fa5d4e311a14dba2cf54615f44ee92b0887c4d
dstlmike <dstl_mike1@hotmail.com>
2019-11-29 21:42:34 -0500

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -266,17 +266,8 @@ function helpCmd(dataHash, callback, result) {
 
   if (regex.test(dataHash.request.text)) {
     if (dataHash.isMod) {
-callback(true, "cmd: /date, desc: Current date,
-    cmd: /fun, desc: Enable commands designated as fun commands, mod: true,
-    cmd: /nofun, desc: Disable commands designated as fun commands, mod: true,
-    cmd: /id, desc: Notifies the requester of their GroupMe ID,
-    cmd: /about, desc: Responds with a short message about the bot,
-    cmd: /go, desc: Send a test email to bot, mod: true,
-    cmd: /nest18, desc: Set Nest temperature to 18 degrees celsius,
-    cmd: /nest19, desc: Set Nest temperature to 19 degrees celsius,
-    cmd: /nest20, desc: Set Nest temperature to 20 degrees celsius,
-    cmd: /nest21, desc: Set Nest temperature to 21 degrees celsius,
-    cmd: /nest22, desc: Set Nest temperature to 22 degrees celsius", []);
+callback(true, "cmd: /date, desc: Current date" + \n + "cmd: /fun, desc: Enable commands designated as fun commands, mod: true,", []);
+   
 
     //callback(true, "Command List ", {}, []);
 
