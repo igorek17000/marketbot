0d2bc42144a6e7e9d37194aa79cda4c8c8158b21
dstlmike <dstl_mike1@hotmail.com>
2019-12-01 03:09:37 -0500

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -332,9 +332,9 @@ var date = moment().utcOffset(-300).format('LLLL');
 const chalk = require('chalk'); 
 const sliceAnsi = require('slice-ansi'); 
 const input = "The quick brown " + chalk.red("fox jumped over ") + 	"the lazy " + chalk.green("dog and then ran away with the unicorn."); 
-console.log(sliceAnsi(input));
+console.log(input);
 
-callback(true, "Hello " + sliceAnsi(input), []);
+callback(true, "Hello " + input, []);
   
 } else {
 return false;
