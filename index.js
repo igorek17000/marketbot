6240113e942205fa658b8b17a85fccb19110b518
dstlmike <dstl_mike1@hotmail.com>
2019-12-10 21:55:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ server = http.createServer(function (req, res) {
 });
 
 port = Number(process.env.NODEJS_SERVICE_PORT || process.env.PORT || 8080);
-ip = process.env.NODEJS_SERVICE_IP || "127.0.0.1" || "0.0.0.0";
+ip = process.env.NODEJS_SERVICE_IP || "0.0.0.0";
 
 server.listen(port, ip);
 
