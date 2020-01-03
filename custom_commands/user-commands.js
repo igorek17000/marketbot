f2a549b7adc3cdecaa1d1be8bff69ad45847079f
dstlmike <dstl_mike1@hotmail.com>
2020-01-02 22:07:35 -0500

    Update user-commands.js

--- custom_commands/user-commands.js
+++ custom_commands/user-commands.js
@@ -307,11 +307,12 @@ function modCommandCmd(request, bots, isMod, callback) {
       callback(true, msg, []);
       return msg;
     }
-    val[2] = "true";
+    
     for (cmd in commands) {
+      val[2] = "true";
       if (commands[cmd].name == val[1].toLowerCase()) {
        
-        commands[cmd]["mod"] = val[2];
+        commands[cmd]["mod"] = ["true"];
         modCommandCmdDB(commands[cmd]);
 
         var msg = val[1] + " Command has been updated";
