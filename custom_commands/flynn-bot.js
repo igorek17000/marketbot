4196591c8e389090f04e5fc56dab3d9898c3376d
dstlmike <dstl_mike1@hotmail.com>
2020-01-11 15:56:54 -0500

    Update flynn-bot.js

--- custom_commands/flynn-bot.js
+++ custom_commands/flynn-bot.js
@@ -82,7 +82,7 @@ exports.checkCommands = function(dataHash, callback) {
     } else if (!flynnbot[flynnb].saturday) {
       msg = "Sunday\n" + flynnb.sunday + "\n" + "Monday\n" + flynnb.monday + "\n" + "Tuesday\n" + flynnb.tuesday + "\n" + "Wednesday\n" + flynnb.wednesday+  "\n" + "Thursday\n" + flynnb.thursday + "\n" + "Friday\n" + flynnb.friday;
     } else {
-      msg = "Sunday\n" + flynnb.sunday + "\n" + "Monday\n" + flynnb.monday + "\n" + "Tuesday\n" + flynnb.tuesday + "\n" + "Wednesday\n" + flynnb.wednesday + "\n" + "Thursday\n" + flynnb.thursday + "\n" + "Friday\n" + flynnb.friday + "\n + "Saturday\n" + flynnb.saturday;
+      msg = "Sunday\n" + flynnb.sunday + "\n" + "Monday\n" + flynnb.monday + "\n" + "Tuesday\n" + flynnb.tuesday + "\n" + "Wednesday\n" + flynnb.wednesday + "\n" + "Thursday\n" + flynnb.thursday + "\n" + "Friday\n" + flynnb.friday + "\n" + "Saturday\n" + flynnb.saturday;
     
          callback(true, msg, []);
         
