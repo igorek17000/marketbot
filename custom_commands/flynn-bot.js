2e3457cd527e82e6e77266badd902bafb81d7e0b
dstlmike <dstl_mike1@hotmail.com>
2020-01-11 16:43:12 -0500

    Update flynn-bot.js

--- custom_commands/flynn-bot.js
+++ custom_commands/flynn-bot.js
@@ -92,7 +92,7 @@ exports.checkCommands = function(dataHash, callback) {
         break;
       }
     }
-  
+  }
 
   for (cmd in flynnBotCommands) {
     var test = flynnBotCommands[cmd](dataHash.request, dataHash.bots, dataHash.isMod, callback);
@@ -100,7 +100,7 @@ exports.checkCommands = function(dataHash, callback) {
       return test;
   }
  }
-}
+
 
 exports.setAll = function(flynnbHash) {
   flynnbot = flynnbHash;
