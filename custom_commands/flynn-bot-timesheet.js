d8e9f60ec440ae111a35f5d662a7752a1876c2be
dstlmike <dstl_mike1@hotmail.com>
2020-01-17 14:30:19 -0500

    Update flynn-bot-timesheet.js

--- custom_commands/flynn-bot-timesheet.js
+++ custom_commands/flynn-bot-timesheet.js
@@ -105,7 +105,7 @@ exports.checkCommands = function(dataHash, callback) {
       msg = "Sunday\n" + flynnb.sunday + "\n" + "Monday\n" + flynnb.monday + "\n" + "Tuesday\n" + flynnb.tuesday + "\n" + "Wednesday\n" + flynnb.wednesday + "\n" + "Thursday\n" + flynnb.thursday + "\n" + "Friday\n" + flynnb.friday + "\n" + "Saturday\n" + flynnb.saturday;
     }
          callback(true, flynnb.name + "\n" + msg, []);
-    break;
+    //break;
       //}
     } else {
        //if (val[1] == "current") {
@@ -152,7 +152,7 @@ exports.checkCommands = function(dataHash, callback) {
 break;
       }
     }
-  }
+  
 
   for (cmd in flynnBotCommands) {
     var test = flynnBotCommands[cmd](dataHash.request, dataHash.bots, dataHash.isMod, callback);
