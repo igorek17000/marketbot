e545037272ab2987f9b3ada0dadd33edafd101c5
dstlmike <dstl_mike1@hotmail.com>
2020-01-17 16:30:14 -0500

    Update flynn-bot-timesheet.js

--- custom_commands/flynn-bot-timesheet.js
+++ custom_commands/flynn-bot-timesheet.js
@@ -73,7 +73,7 @@ exports.checkCommands = function(dataHash, callback) {
 //continue;
 
      var flynnbReg = new RegExp(flynnb.regex, "i"); 
-     if (flynnb.regexcurrent(dataHash.request.text)) {
+     if (flynnb.regexcurrent == dataHash.request.text) {
       flynnb.regex = flynnb.regexcurrent;
 }
 
