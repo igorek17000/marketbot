d352ea4dbf728eecc4333370e94a15e917b4c5ca
dstlmike <dstl_mike1@hotmail.com>
2020-01-20 00:54:48 -0500

    Update rooms.js

--- modules/rooms.js
+++ modules/rooms.js
@@ -43,9 +43,7 @@ exports.getRoom = function(path) {
     room.type = path;
     room.id = rooms[path];
   }
- if (rooms[id] == room.id {
-     botName = rooms[name];
-     }
+    
   return room;
 }
 
