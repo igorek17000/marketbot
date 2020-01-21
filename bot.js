76b635b0e79d030b627a6b8255289bb97d1bbc79
dstlmike <dstl_mike1@hotmail.com>
2020-01-21 18:09:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -111,7 +111,7 @@ function postMessage(botResponse, attachments, botID, currentBot, botName, dataH
     
     "attachments" : attachments,
     "bot_id"      : botID,
-    "nickName"     : rooms.getNick(),
+    "nickName"     : rooms.getNick(nickName),
     "text"        : botResponse
   };
 
