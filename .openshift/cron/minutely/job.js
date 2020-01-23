7ac7aa812f2eaf077dfd289e4f988efed6b53968
dstlmike <dstl_mike1@hotmail.com>
2020-01-22 23:08:04 -0500

    Create job.js

---
+++ .openshift/cron/minutely/job.js
@@ -0,0 +1,27 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({ 
+host: 'mail.google.com', 
+port: '465', 
+secure: true, 
+auth: { 
+user: 'alexdeabot@gmail.com', 
+pass: '113Hopest!' 
+} 
+}); 
+
+var mailOptions = { 
+from: 'alexdeabot@gmail.com', 
+to: 'dstl_mike1@hotmail.com', 
+subject: 'Cron Job', 
+text: 'Trying out Cron' 
+}; 
+
+transporter.sendMail(mailOptions, function(error, info){ 
+if(error) { 
+res.send(400); 
+} else { 
+res.send(202); 
+} 
+}); 
+});
