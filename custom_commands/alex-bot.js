cad4c0051b5723f4a10405f0644e825033a76a30
dstlmike <dstl_mike1@hotmail.com>
2020-02-11 23:22:59 -0500

    Update alex-bot.js

--- custom_commands/alex-bot.js
+++ custom_commands/alex-bot.js
@@ -29,7 +29,7 @@ function updateAlexBotMessage(alexb, callback) {
 }
 
 function updateAlexBotModDate(alexb, callback) {
-  db.updateOneDoc(db_table, {"name": alexb.name}, {$push: { "date": date}}, callback);
+  db.updateOneDoc(db_table, {"name": alexb.name}, {$push: { "date modified": date}}, callback);
 }
 
 exports.checkCommands = function(dataHash, callback) {
