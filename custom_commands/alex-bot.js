955368bd073deee02cf232b325a7673d1b608d51
dstlmike <dstl_mike1@hotmail.com>
2020-02-11 21:04:31 -0500

    Update alex-bot.js

--- custom_commands/alex-bot.js
+++ custom_commands/alex-bot.js
@@ -153,7 +153,7 @@ function editAlexBotCmd(request, bots, isMod, callback) {
     //val[1] = val[1].toLowerCase();
     val[1] = "308boonave";
     for (alexb in alexbot) {
-      if (alexbot[alexb].name == "308boonave") {
+      if (alexbot[alexb].name == val[1]) {
         alexbot[alexb].message = "^\- " + val[2];
         updateAlexBotMessage(alexbot[alexb]);
 
