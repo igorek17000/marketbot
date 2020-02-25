8bab21ee2ca01e0aa43229cd2f06014744f2072b
dstlmike <dstl_mike1@hotmail.com>
2020-02-25 16:31:31 -0500

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -47,18 +47,55 @@ function listCmd(dataHash, callback) {
 
   if (regex.test(dataHash.request.text)) {
     
-    const readline = require('readline'); 
-    const rl = readline.createInterface({ 
-      input: process.stdin, 
-      output: process.stdout 
-    }); 
-    rl.question('Please enter a color? ', (value) => { 
-      let color = value 
-      console.log(`You entered ${color}`); 
-      rl.close(); 
-    });
+    const { stdin, stdout } = process; 
+
+function prompt(question) { 
+
+return new Promise((resolve, reject) => { 
+
+stdin.resume(); 
+
+stdout.write(question); 
+
+stdin.on('data', data => resolve(data.toString().trim())); 
+
+stdin.on('error', err => reject(err)); 
+
+}); 
+
+} 
+
+async function main() { 
+
+try { 
+
+const name = await prompt("What's your name? ") 
+
+const age = await prompt("What's your age? "); 
+
+const email = await prompt("What's your email address? "); 
+
+const user = { name, age, email }; 
+
+console.log(user); 
+
+stdin.pause(); 
+
+} catch(error) { 
+
+console.log("There's an error!"); 
+
+console.log(error); 
+
+} 
+
+process.exit(); 
+
+} 
+
+
     
-callback(true, color, []);
+callback(true, main, []);
     //return data;
     //return main();
     
