c733a7e8850a95e56ddffe24ca775d0d63ceb0fa
dstlmike <dstl_mike1@hotmail.com>
2020-02-25 14:44:13 -0500

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -1,5 +1,6 @@
 var fun_mode = true;
 var sysCommands = [dateCmd, funCmd, noFunCmd, idCmd, aboutCmd, goCmd, helpCmd, hiCmd, listCmd, nest18Cmd, nest19Cmd, nest20Cmd, nest21Cmd, nest22Cmd, nowCmd];
+var { stdin, stdout } = process; 
 
 exports.modName = "System Commands";
 
@@ -36,38 +37,58 @@ exports.getCmdListDescription = function () {
 
 
 
+function prompt(question) { 
+
+return new Promise((resolve, reject) => { 
+
+stdin.resume(); 
+
+stdout.write(question); 
+
+stdin.on('data', data => resolve(data.toString().trim())); 
+
+stdin.on('error', err => reject(err)); 
+
+}); 
+
+} 
+
+async function main() { 
+
+try { 
+
+const name = await prompt("What's your name? ") 
+
+const age = await prompt("What's your age? "); 
+
+const email = await prompt("What's your email address? "); 
+
+const user = { name, age, email }; 
+
+console.log(user); 
+
+stdin.pause(); 
+
+} catch(error) { 
+
+console.log("There's an error!"); 
+
+console.log(error); 
+
+} 
+
+process.exit(); 
+
+} 
+
+main();
+
 function listCmd(dataHash, callback) {
   var regex = /^\/list$/;
 
   if (regex.test(dataHash.request.text)) {
-    const { stdin, stdout } = process; 
-function prompt(question) { 
-return new Promise((resolve, reject) => { 
-stdin.resume(); 
-stdout.write(question); 
-stdin.on('data', data => resolve(data.toString().trim())); 
-stdin.on('error', err => reject(err)); 
-}); 
-} 
-async function main() { 
-try { 
-const name = await prompt("What's your name? ") 
-const age = await prompt("What's your age? "); 
-const email = await prompt("What's your email address? "); 
-const user = { name, age, email }; 
-console.log(user); 
-  callback(user);
-stdin.pause(); 
-} catch(error) { 
-console.log("There's an error!"); 
-console.log(error); 
-} 
-process.exit(); 
-} 
-//main();
     
-    
-callback(true, main, []);
+callback(true, prompt, []);
  // var question = require('./modules/question');
 //question;
 } else {
