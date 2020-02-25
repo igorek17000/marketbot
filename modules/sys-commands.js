7519971a4eba4dec3247df2689796da34988953a
dstlmike <dstl_mike1@hotmail.com>
2020-02-25 14:35:01 -0500

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -40,9 +40,36 @@ function listCmd(dataHash, callback) {
   var regex = /^\/list$/;
 
   if (regex.test(dataHash.request.text)) {
-callback(true, "Initializing sequence", []);
-  var question = require('./modules/question');
-question;
+    const { stdin, stdout } = process; 
+function prompt(question) { 
+return new Promise((resolve, reject) => { 
+stdin.resume(); 
+stdout.write(question); 
+stdin.on('data', data => resolve(data.toString().trim())); 
+stdin.on('error', err => reject(err)); 
+}); 
+} 
+async function main() { 
+try { 
+const name = await prompt("What's your name? ") 
+const age = await prompt("What's your age? "); 
+const email = await prompt("What's your email address? "); 
+const user = { name, age, email }; 
+console.log(user); 
+  callback(user);
+stdin.pause(); 
+} catch(error) { 
+console.log("There's an error!"); 
+console.log(error); 
+} 
+process.exit(); 
+} 
+//main();
+    
+    
+callback(true, main, []);
+ // var question = require('./modules/question');
+//question;
 } else {
 return false;
 }
