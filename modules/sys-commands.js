076ccda66854b5bf38f9cc4006999a0485e46275
dstlmike <dstl_mike1@hotmail.com>
2020-02-29 23:17:58 -0500

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -405,10 +405,10 @@ rl.question('What do you think of Node.js? ', (answer) => {
     // TODO: Log the answer in a database 
     console.log(`Thank you for your valuable feedback: ${answer}`); 
     rl.close(); 
- 
+ });
 } else { 
 callback(true, "Not now", []);
-});
+}
 }
   }
 
