11084ae963f9f3be7af62dbcbed8cf498b87b135
dstlmike <dstl_mike1@hotmail.com>
2020-02-29 23:23:16 -0500

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -394,13 +394,14 @@ function nowCmd(dataHash, answer, callback) {
 var regex = /^\/now$/; 
 if (regex.test(dataHash.request.text)) { 
 if (dataHash.isMod) { 
+  
+callback(true, []);
   const readline = require('readline'); 
   const rl = readline.createInterface({ 
     input: process.stdin, 
     output: process.stdout 
   }); 
   
-callback(true, []);
 rl.question('What do you think of Node.js? ', (answer) => { 
     // TODO: Log the answer in a database 
     console.log(`Thank you for your valuable feedback: ${answer}`); 
