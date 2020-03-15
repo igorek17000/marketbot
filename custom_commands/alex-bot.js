a761cc020219f9299640693bae2526f04c1eaff1
dstlmike <dstl_mike1@hotmail.com>
2020-03-15 05:10:49 -0400

    Update alex-bot.js

--- custom_commands/alex-bot.js
+++ custom_commands/alex-bot.js
@@ -206,7 +206,7 @@ function sendingAlexBotCmd(request, callback) {
 //var moment = require('moment'); 
 
 //var date2 = moment().utcOffset(-300).format('LLLL');
-var done = new request.text;
+var done = new dataHash.request;
 var word = "/sendon";
 
 if (regex.test(reqText)){
