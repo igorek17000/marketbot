25542ff246811467a791122bc4fba5e8c98a3f53
dstlmike <dstl_mike1@hotmail.com>
2020-03-21 17:24:30 -0400

    Update alex-bot.js

--- custom_commands/alex-bot.js
+++ custom_commands/alex-bot.js
@@ -6,7 +6,7 @@ var moment = require('moment');
 var date = moment().utcOffset(-300).format('LLLL');
 var alexBotCommands = [addAlexBotCmd, describeAlexBotCmd, editAlexBotCmd];
 var db = require('../modules/db.js');
-var http = require('http');
+var http, require('http');
 
 var, server, ip, router;
 server = http.createServer(function (req, res) {
