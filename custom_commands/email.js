9f8ca8998e70ac63e106908a3735a8a49626c803
dstlmike <dstl_mike1@hotmail.com>
2020-03-25 22:58:24 -0400

    Update email.js

--- custom_commands/email.js
+++ custom_commands/email.js
@@ -222,8 +222,8 @@ function sendEmailCmd(request, bots, isMod, callback) {
 
     for (cmd in commands) {
       if (commands[cmd].status = "draft") {
-       var msg = "Email sent.";
-        callback(true, msg, []);
+       cmd.status = "Email sent.";
+        //callback(true, msg, []);
 }
 var nodemailer = require('nodemailer');
 
