221ded25ec3322b06c0cf8d54ea36cc13b09e9f1
dstlmike <dstl_mike1@hotmail.com>
2020-03-31 01:50:52 -0400

    Update email.js

--- custom_commands/email.js
+++ custom_commands/email.js
@@ -69,17 +69,17 @@ exports.checkCommands = function(dataHash, callback) {
 
      //var flynnbcReg = new RegExp(flynnb.regexcurrent, "i");  
         
-     //if (cmd.status == "draft" && dataHash.request.text && cmdReg.test(dataHash.request.text)){
+     if (dataHash.request.text && cmdReg.test(dataHash.request.text)){
       //  var val = cmdReg.exec(dataHash.request.text);
       
      // } else if (flynnbc.bots.indexOf(dataHash.currentBot.type) > -1 && dataHash.request.text && flynnbcReg.test(dataHash.request.text)){
         //var val = flynnbcReg.exec(dataHash.request.text);
    
 
-      if (cmd.bots.indexOf(dataHash.currentBot.type) > -1 && dataHash.request.text && cmdReg.test(dataHash.request.text)){
+      //if (cmd.bots.indexOf(dataHash.currentBot.type) > -1 && dataHash.request.text && cmdReg.test(dataHash.request.text)){
         var val = cmdReg.exec(dataHash.request.text);
 
-var msg = "";
+//var msg = "";
           //if (callback) {
           
          callback(true, cmd.attachments, []);
