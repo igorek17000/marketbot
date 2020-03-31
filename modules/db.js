c5f565f6079b86c246959183253fa06e3776ca21
dstlmike <dstl_mike1@hotmail.com>
2020-03-30 23:42:01 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -42,7 +42,7 @@ function connect(callback){
 
 exports.getAllDocuments = function(collection, callback) {
   mongoDB.connect(connection_string, function(err, db) {
-    if(err) throw err;
+    if(err) callback(err); //throw err;
     var allDocs = db.collection(collection).find().toArray(function(err, docs) {
       callback(docs);
       //db.close();
