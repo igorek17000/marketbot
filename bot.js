440187d653bf352d4c40986605c2ba4d4faab8a3
dstlmike <dstl_mike1@hotmail.com>
2020-03-30 23:02:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -135,7 +135,7 @@ function postMessage(botResponse, attachments, botID, nickName) {
 
 
 botReq = HTTPS.request(options, function(res) { 
-console.log('Status: ' + res.statusMessage + ', Status code: ' + res.statusCode + ' Message: ' + res.body,)
+console.log('Status: ' + res.statusMessage + ', Status code: ' + res.statusCode + ' Message: ' + res.json())
 
 
       //if (res.statusCode == 200) || (res.statusCode == 202) {
