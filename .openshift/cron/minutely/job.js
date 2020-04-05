811260584b9bafd2708a44f231369e5c37564720
dstlmike <dstl_mike1@hotmail.com>
2020-04-05 02:37:42 -0400

    Update job.js

--- .openshift/cron/minutely/job.js
+++ .openshift/cron/minutely/job.js
@@ -1,33 +1,3 @@
-var cron = require("node-cron"); 
-var express = require("express"); 
 
+console.log('Running Cron Job');
 
-var nodemailer = require('nodemailer');
-
-var transporter = nodemailer.createTransport({ 
-host: 'mail.google.com', 
-port: '465', 
-secure: true, 
-auth: { 
-user: 'alexdeabot@gmail.com', 
-pass: '113Hopest!' 
-} 
-}); 
-
-cron.schedule("1 * * * *", function(){
-var mailOptions = { 
-from: 'alexdeabot@gmail.com', 
-to: 'dstl_mike1@hotmail.com', 
-subject: 'Cron Job', 
-text: 'Trying out Cron' 
-}; 
-
-transporter.sendMail(mailOptions, function(error, info){ 
-if(error) { 
-res.send(400); 
-} else { 
-res.send(202); 
-} 
-}); 
-});
-});
