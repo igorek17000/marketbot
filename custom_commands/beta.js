d92dc98ff605a88e9a6454a361f5875a6e2f0fc7
dstlmike <dstl_mike1@hotmail.com>
2020-04-05 21:41:42 -0400

    Update beta.js

--- custom_commands/beta.js
+++ custom_commands/beta.js
@@ -34,3 +34,33 @@ app.listen(3128);
 
 
 
+// create mail transporter 
+let transporter = nodemailer.createTransport({ 
+service: "gmail", 
+auth: { 
+user: "COMPANYEMAIL@gmail.com", 
+pass: "userpass" 
+} 
+}); 
+// sending emails at periodic intervals 
+cron.schedule("* * * * Wednesday", function(){ 
+console.log("---------------------"); 
+console.log("Running Cron Job"); 
+
+let mailOptions = { 
+from: "COMPANYEMAIL@gmail.com", 
+to: "sampleuser@gmail.com", 
+subject: `Not a GDPR update ;)`, 
+text: `Hi there, this email was automatically sent by us` 
+}; 
+
+transporter.sendMail(mailOptions, function(error, info) { 
+if (error) { 
+throw error; 
+} else { 
+console.log("Email successfully sent!"); 
+} 
+}); 
+}); 
+
+app.listen("3128");
