73892ae1a640d0eaeda28c2e0a426828d5fb4702
dstlmike <dstl_mike1@hotmail.com>
2020-04-09 18:15:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,8 @@ var rooms        = require('./modules/rooms');
 //commands with custom actions
 var alexBot      = require('./custom_commands/alex-bot');
 var flynnBot     = require('./custom_commands/flynn-bot-timesheet');
-var emailBot      = require('./custom_commands/email');
+var emailBot     = require('./custom_commands/email');
+var birthdayBot  = require('./custom_commands/birthdays');
 var userCmds     = require('./custom_commands/user-commands');
 var userMentions = require('./custom_commands/user-mentions');
 var sysTriggers  = require('./custom_commands/system-triggers');
@@ -34,7 +35,7 @@ var config       = require('./config/config');
 var HTTPS        = require('https');
 
 //Temporarily just an array of the commands functions. Make an object with configuration values.
-var checkCommandsHSH = [alexBot, flynnBot, emailBot, mods, sysTriggers, userCmds, userMentions, sysCommands, atEveryone, funCommands, quotes, rooms, gif, catFact, urbanDict];
+var checkCommandsHSH = [alexBot, flynnBot, emailBot, birthdayBot, mods, sysTriggers, userCmds, userMentions, sysCommands, atEveryone, funCommands, quotes, rooms, gif, catFact, urbanDict];
 
 exports.init = function() {
   var req = this.req;
