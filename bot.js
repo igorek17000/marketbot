c3dacf148c9d0b66a0984fc7d96914e0f53d5b12
dstlmike <dstl_mike1@hotmail.com>
2020-04-10 19:38:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -191,22 +191,22 @@ var logID = 'b6c42cc2a1bee3c38f07723d78';
 
     "attachments" : attachments,
     "bot_id"      : logID,
-   // "text"        : logName,
-    "text"        : "Hello there" //logResponse
+    "text"        : logName,
+    "text"        : logResponse
     //"name"        : logName
   };
 
 logName = '';
 if (logID == 'b6c42cc2a1bee3c38f07723d78') {
 
-           logName = 'Config';
+           logName = '"Config"';
 
            } else if (logID == '282865de8ce30137567238148f') {
-           logName = '308BoonBot';
+           logName = '"308BoonBot"';
            } else if (logID == '8631a4c35f0f0f250bd5d46f44') {
-           logName = 'FlynnBot';
+           logName = '"FlynnBot"';
            } else if (logID == '2184cee4d169628e83e82ee05f') {
-           logName = 'AshleyBot';
+           logName = '"AshleyBot"';
            } else {
              logName = logID;
              
