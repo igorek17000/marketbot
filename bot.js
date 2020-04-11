7415353c3ce59701bc579807c3503cb95b23bb3f
dstlmike <dstl_mike1@hotmail.com>
2020-04-10 22:39:32 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -202,7 +202,7 @@ var botName;
 //botName = dataHash.request.name;
 //}
 //var logName = '';
-var botID = rooms.getRoom(botRoom).id;
+var botID = rooms.getRoom(botRoom);
 if (botID == 'b6c42cc2a1bee3c38f07723d78') {
 
            logName = "Config";
