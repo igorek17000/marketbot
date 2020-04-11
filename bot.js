268df64e20afb327576a9f43ece3d8783391194b
dstlmike <dstl_mike1@hotmail.com>
2020-04-10 20:11:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -192,11 +192,11 @@ var logID = 'b6c42cc2a1bee3c38f07723d78';
     "attachments" : attachments,
     "bot_id"      : logID,
    // "text"        : logName
-    "text"        : logName + logResponse
+    "text"        : """ + logName + """ + logResponse
     //"name"        : logName
   };
 
-var logName;
+var logName = "";
 if (logID == 'b6c42cc2a1bee3c38f07723d78') {
 
            logName = "Config";
