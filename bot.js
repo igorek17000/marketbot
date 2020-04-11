f7eaebe07eef0d211e58eb8685451eb4650b41cd
dstlmike <dstl_mike1@hotmail.com>
2020-04-11 00:42:16 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -203,7 +203,7 @@ var logName = '';
            } else {
              logName = botID;
              }
-getAllRooms();
+//getAllRooms();
  // console.log('sending response to ' + nickName + '\n' + botResponse);
 
   //console.log('sending response to ' + logName + '\n' + logResponse + res.message);
