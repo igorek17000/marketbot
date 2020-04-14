39dba07fd39296155914a98dcd934778718bc2c2
dstlmike <dstl_mike1@hotmail.com>
2020-04-13 23:44:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -163,12 +163,7 @@ var nickName = '';
 
 
 
-body1 = {
-    
-    "attachments" : attachments,
-    "bot_id"      : logID,
-    "text"        : logName + botResponse
-  };
+
 
      logName;
       if (botID == 'b6c42cc2a1bee3c38f07723d78') {
@@ -185,6 +180,7 @@ body1 = {
   console.log('sending response to ' + logName + '\n' + botResponse);
 }
 
+
 botReq = HTTPS.request(options, function(res) { 
 console.log('Status: ' + res.statusMessage + ' Status code: ' + res.statusCode)
 
@@ -195,21 +191,6 @@ console.log('Status: ' + res.statusMessage + ' Status code: ' + res.statusCode)
         //console.log('rejecting bad status code ' + res.statusCode);
       //}
   });
-
-
-
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-
-  //botReq.end(JSON.stringify(body))
-
-//continue;
-
 
 logReq = HTTPS.request(options, function(res) { 
 console.log('Status: ' + res.statusMessage + ', Status code: ' + res.statusCode)
@@ -233,7 +214,24 @@ console.log('Status: ' + res.statusMessage + ', Status code: ' + res.statusCode)
 
 //body = body1;
 //botReq.end(JSON.stringify(body));
-  logReq.end(JSON.stringify(body1));
+  //logReq.end(JSON.stringify(body1));
+
+
+
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+botReq.on('end', function(res) {
+logReq.end(JSON.stringify(body1));
+};
+  botReq.end(JSON.stringify(body));
+
+//continue;
+
 
 
 }
