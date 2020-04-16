0fa120a1fbb5e20fb3908e3528188c8483cb04fe
dstlmike <dstl_mike1@hotmail.com>
2020-04-16 01:33:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -201,7 +201,7 @@ var nickName = '';
 
 
 botReq = HTTPS.request(options, function(res) { 
-console.log('Status: ' + res.statusMessage + ' Status code: ' + res.statusCode + '\n' + res.metaText)
+console.log('Status: ' + res.statusMessage + ' Status code: ' + res.statusCode)
 
 
       //if (res.statusCode == 200) || (res.statusCode == 202) {
