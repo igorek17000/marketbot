34bf7ae96a40515c5db0195dc69d0ebb0439bfb5
dstlmike <dstl_mike1@hotmail.com>
2020-04-22 01:29:32 -0400

    Update postbot.js

--- custom_commands/postbot.js
+++ custom_commands/postbot.js
@@ -203,14 +203,14 @@ function cmdUrbanRnd(funMode, request, callback){
 */
 
 
-function findWeather(err, result, callback) {
+function findWeather(result, callback) {
 find({search: 'Toronto, ON', degreeType: 'C'}, function(err, result) {   
 if(err) 
 console.log(err);     
 console.log(JSON.stringify(result, null, 2));
 callback(result);
 return result; 
-});
+}
 }
 
 
