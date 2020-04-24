cbe6116715fcff540b5d4e64d3bbe0ebfd59d5ba
dstlmike <dstl_mike1@hotmail.com>
2020-04-24 01:47:51 -0400

    Update toronto.js

--- modules/toronto.js
+++ modules/toronto.js
@@ -13,6 +13,7 @@ weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, resul
 //exports.toronto = function(result, callback) { 
 var find = weather.find({search: 'Toronto, ON', degreeType: 'C'}, function(err, callback) { 
 if(err) throw err; 
+var result = [];
 //var callback = callback;
 callback(result);
 });
