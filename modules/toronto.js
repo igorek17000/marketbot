2ec05dd20593cde2a6e1776725bef61ee116598c
dstlmike <dstl_mike1@hotmail.com>
2020-04-24 03:11:07 -0400

    Update toronto.js

--- modules/toronto.js
+++ modules/toronto.js
@@ -1,11 +1,11 @@
-let request = require('request'); 
-let apiKey = '***********************************';
-let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}` 
+var request = require('request'); 
+var apiKey = '3ff9b16eb3019ed16c69ce87658e04d3';
+var city = 'toronto';
+var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}` 
 request(url, function (err, response, body) { 
 if(err){ 
 console.log('error:', error); 
 } else { 
-let weather = JSON.parse(body) 
-let message = `It's ${weather.main.temp} degrees in ${weather.name}!`; 
+var weather = JSON.parse(body) 
+var message = `It's ${weather.main.temp} degrees in ${weather.name}!`; 
 console.log(message); }});
