3a3c90b09e86b124863dfcf5a5c5d6a65b3a466a
dstlmike <dstl_mike1@hotmail.com>
2020-04-24 00:27:45 -0400

    Create toronto.js

---
+++ modules/toronto.js
@@ -0,0 +1,28 @@
+var weather = require('weather-js');
+
+/*weather.find({search: 'Toronto, ON', degreeType: 'C'}, function(err, result) { 
+if(err) console.log(err); 
+console.log(JSON.stringify(result, null, 2)); //callback(result);//});
+
+
+
+weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) { if(err) console.log(err); console.log(JSON.stringify(result, null, 2)); });
+*/
+
+
+exports.toronto = function(find, callback) { 
+weather.find({search: 'Toronto, ON', degreeType: 'C'}, function(err, result) { 
+if(err) throw err; 
+var callback = callback;
+callback(result);
+});
+}
+
+/*mongoDB.connect(connection_string, function(err, db) { 
+if(err) throw err; 
+var allDocs = db.collection(collection).find().toArray(function(err, docs) { 
+callback(docs); //db.close(); 
+}); 
+});
+}
+*/
