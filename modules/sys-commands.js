621e27975afad80a5f5679c2b6f5280a0585d7ca
dstlmike <dstl_mike1@hotmail.com>
2020-04-24 01:22:11 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -119,7 +119,7 @@ return false;
 
 //---------
 
-function weatherCmd(dataHash, callback) {
+function weatherCmd(dataHash, result, callback) {
   var regex = /^\/weathertoronto$/;
 
 var moment = require('moment'); 
