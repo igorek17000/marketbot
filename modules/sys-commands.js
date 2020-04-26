aba2fd910216e007f3b34e763fbca80efe484898
dstlmike <dstl_mike1@hotmail.com>
2020-04-25 22:36:05 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -152,8 +152,9 @@ var weather = JSON.parse(body)
 
 
 var message = `It's ${weather.main.temp} degrees in ${weather.name}!`; 
+var resWeather = 
 
-var msg = "Current weather in " + weather.name + "\n" + "Currently: " + weather.main.temp + " degrees C" + "\n" + "Mainly: " + weather.weather[0].description + "\n" + "High: " + weather.main.temp_max + " C"+ "\n" + "Low: " + weather.main.temp_min + " C" + "\n" + "Relative humidity: " + weather.main.humidity + "!"; 
+var msg = "Current weather in " + weather.name + "\n" + "Currently: " + weather.main.temp + " degrees C" + "\n" + "Mainly: " + weather.weather[0].description + "\n" + "High: " + weather.main.temp_max + " C"+ "\n" + "Low: " + weather.main.temp_min + " C" + "\n" + "Relative humidity: " + weather.main.humidity + "\n" + "Wind: " + weather.wind.speed + " km/h" + "\n" + "Wind direction: " + weather.wind.deg + "!"; 
 console.log(message);
 callback(true, msg, []);
 }
