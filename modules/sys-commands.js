333202a3c124b2ea76baa4ab06f624f776493a59
dstlmike <dstl_mike1@hotmail.com>
2020-04-26 00:18:29 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -147,6 +147,10 @@ console.log('error:', error);
 } else { 
 
 var weather = JSON.parse(body);
+var message = `It's ${weather.main.temp} degrees in ${weather.name}!`; 
+
+
+var msg = "Current weather in " + weather.name + "\n" + "Currently: " + weather.main.temp + " degrees C" + "\n" + "Mainly: " + weather.weather[0].description + "\n" + "High: " + weather.main.temp_max + " C"+ "\n" + "Low: " + weather.main.temp_min + " C" + "\n" + "Relative humidity: " + weather.main.humidity + "\n" + "Wind: " + weather.wind.speed + " km/h" + "\n" + "Wind direction: " + weather.wind.deg + direction; 
 
 
 if (weather.wind.deg == 350 || 360 || 10) {
@@ -189,10 +193,10 @@ direction = "N/NW";
 
 
 
-var message = `It's ${weather.main.temp} degrees in ${weather.name}!`; 
+//var message = `It's ${weather.main.temp} degrees in ${weather.name}!`; 
 
 
-var msg = "Current weather in " + weather.name + "\n" + "Currently: " + weather.main.temp + " degrees C" + "\n" + "Mainly: " + weather.weather[0].description + "\n" + "High: " + weather.main.temp_max + " C"+ "\n" + "Low: " + weather.main.temp_min + " C" + "\n" + "Relative humidity: " + weather.main.humidity + "\n" + "Wind: " + weather.wind.speed + " km/h" + "\n" + "Wind direction: " + weather.wind.deg + direction; 
+//var msg = "Current weather in " + weather.name + "\n" + "Currently: " + weather.main.temp + " degrees C" + "\n" + "Mainly: " + weather.weather[0].description + "\n" + "High: " + weather.main.temp_max + " C"+ "\n" + "Low: " + weather.main.temp_min + " C" + "\n" + "Relative humidity: " + weather.main.humidity + "\n" + "Wind: " + weather.wind.speed + " km/h" + "\n" + "Wind direction: " + weather.wind.deg + direction; 
 console.log(message);
 
 
