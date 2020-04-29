3cf970789544df3db8b9b887412371b23a3ec5bd
dstlmike <dstl_mike1@hotmail.com>
2020-04-29 11:34:12 -0400

    Update toronto.js

--- modules/toronto.js
+++ modules/toronto.js
@@ -1,4 +1,4 @@
-var request = require('request'); 
+/*var request = require('request'); 
 var apiKey = '3ff9b16eb3019ed16c69ce87658e04d3';
 var city = 'toronto';
 var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}` 
@@ -14,3 +14,12 @@ var msg = "It's ${weather.main.temp} degrees in ${weather.name}!";
 console.log(message); 
 callback(msg);
 }});
+*/
+
+
+
+var geolocation = require('geolocation');
+geolocation.getCurrentPosition(function (err, position) { 
+if (err) throw err 
+console.log(position) 
+})
