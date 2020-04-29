a16fa2bed28a0b2118657d706431ea971e31eddb
dstlmike <dstl_mike1@hotmail.com>
2020-04-29 13:41:05 -0400

    Update toronto.js

--- modules/toronto.js
+++ modules/toronto.js
@@ -39,7 +39,7 @@ var request = require('request');
 var options = { 
 method: 'GET', 
 url: 'https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/', 
-qs: {ip: '2605%3A8d80%3A605%3Aa002%3A91a7%3A836d%3A52a5%3A5564'}, 
+qs: {ip: '72.143.201.34}, 
 headers: { 
 'x-rapidapi-host': 'ip-geolocation-ipwhois-io.p.rapidapi.com', 
 'x-rapidapi-key': 'b0eb0323cdmsh4acdf0a8b6ee640p12bbf5jsn5cc6fcc144b3' 
