b381b1409b0c96b307ce16d4f8abfc9de6bcc187
dstlmike <dstl_mike1@hotmail.com>
2020-04-29 04:38:42 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -158,7 +158,7 @@ console.log('error:', error);
 var weather = JSON.parse(body);
 var oneof = "";
 oneof += body;
-var oneoff = JSON.parse(oneof);
+//var oneoff = JSON.parse(oneof);
 //var message = `It's ${weather.main.temp} degrees in ${weather.name}!`; 
 
 
@@ -209,7 +209,7 @@ var message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
 
 
 //var msg = weather.name + " weather at " + weather.dt_txt + "\n" + "Currently: " + weather.main.temp + "° celsius" + "\n" + "Mainly: " + weather.weather[0].description + "\n" + "High: " + weather.main.temp_max +  "° celcius" + "\n" + "Low: " + weather.main.temp_min + "° celsius" + "\n" + "Relative humidity: " + weather.main.humidity + "\n" + "Wind: " + weather.wind.speed + " km/h" + "\n" + "Wind direction: Blowing from the " + direction + " bearing " + weather.wind.deg + "° degrees."; 
-var msg = oneoff;
+var msg = oneof;
 console.log(message);
 
 
