4f76a76e184438375f78082cbb4ae97b511fba1e
dstlmike <dstl_mike1@hotmail.com>
2020-05-06 07:00:12 -0400

    Update api.js

--- custom_commands/api.js
+++ custom_commands/api.js
@@ -192,7 +192,7 @@ logID = "b6c42cc2a1bee3c38f07723d78";
 
     options = {
     hostname: 'api.groupme.com',
-    path: '/v3/bots, //bots/post',
+    path: '/v3/bots', //bots/post',
     method: 'GET'
   };
 
