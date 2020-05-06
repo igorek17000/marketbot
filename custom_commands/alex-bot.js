a0d929886414072c4c7ab61910efa0599924547c
dstlmike <dstl_mike1@hotmail.com>
2020-05-05 20:58:23 -0400

    Update alex-bot.js

--- custom_commands/alex-bot.js
+++ custom_commands/alex-bot.js
@@ -246,21 +246,83 @@ function editAlexBotCmd(request, bots, isMod, callback) {
 
 //------------
 function restartCmd(request, isMod, callback) {
-  var regex = /^\/restart/i;
+  var regex = /^\/restart (.+?) ([\s\S]+)/i;
   var reqText = request.text;
+  var options, body, logReq, logID, msg;
   if (regex.test(reqText)){
     if (!isMod) {
      callback(true, "Sorry I'm no fun right now.", []);
      return "Sorry I'm no fun right now.";
     }
 
+if (val[1] == "text") {
+  msg = val[2];
 
 
+
+//function cmdPost(msg, attachments, logID) {
+  //var options, body, logReq, logID;
+//botID = botID;
+logID = "b6c42cc2a1bee3c38f07723d78";
+//var msg = val[1];
+
+
+
+
+    options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+
+    body = {
+    
+    "attachments" : "attachments",
+    "bot_id"      : logID, 
+    "text"        : msg
+  };
+
+
+
+
+  console.log('sending response to text object ' + '\n' + msg);
+
+
+
+logReq = HTTPS.request(options, function(res) { 
+console.log(' Status: ' + res.statusMessage + ' Status code: ' + res.statusCode)
+
+
+      //if (res.statusCode == 200) || (res.statusCode == 202) {
+        //neat
+//} else {
+        //console.log('rejecting bad status code ' + res.statusCode);
+      //}
+  });
+
+
+
+
+
+  logReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  logReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  logReq.end(JSON.stringify(body));
+
+
+
+var msg = "Posting message\n";
+callback(true, "msg", []);
 //var msg = require('./postbot');
-//callback(true, "One test\n", []);
-var msg = require('./postbot');
-msg;
-       } else {
+return msg;
+
+  }
+       
+} else {
     
 
 
