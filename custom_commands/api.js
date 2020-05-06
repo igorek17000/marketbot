240a7a9574096e8cc76793001b8c09fef38088fd
dstlmike <dstl_mike1@hotmail.com>
2020-05-06 01:02:05 -0400

    Update api.js

--- custom_commands/api.js
+++ custom_commands/api.js
@@ -284,6 +284,6 @@ msg;
 
 
 
-callback = function(response) { response.on('data', function (chunk) { str += chunk; }); response.on('end', function () { console.log(req.data); console.log(str); // your code here if you want to use the results ! }); } var req = http.request(options, callback).end();
+//callback = function(response) { response.on('data', function (chunk) { str += chunk; }); response.on('end', function () { console.log(req.data); console.log(str); // your code here if you want to use the results ! }); } var req = http.request(options, callback).end();
 
 
