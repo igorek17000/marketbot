60f9879972161694c2c0ef9f817bbab0cc8e2286
dstlmike <dstl_mike1@hotmail.com>
2020-05-13 22:28:19 -0400

    Update postbot.js

--- custom_commands/postbot.js
+++ custom_commands/postbot.js
@@ -1,3 +1,13 @@
+
+
+var HTTPS = require('https');
+
+exports.modName = "Post module";
+
+logID = "b6c42cc2a1bee3c38f07723d78";
+var msg = response;
+
+
 options = {
     hostname: 'api.groupme.com',
     path: '/v3/groups?token=nQMmW22iRiJY4TL3W3aoWaCbNpkGO0yIVAaHRvaR',
@@ -21,33 +31,90 @@ body = {
 
 
 
-/*
-    body = {
-    
-    "attachments" : "attachments",
-    "bot_id"      : logID
-    //"text"        : response
-  };
-*/
 
 callback = function(response) { 
 var str = '';
+
+var url = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+
+var body = {
+    
+    "attachments" : "attachments",
+    "bot_id"      : logID,
+    "text"        : ""
+  };
+
 response.on('data', function (chunk) { 
 str += chunk; 
 }); 
 response.on('end', function () { 
 console.log(req.data); 
 console.log(str); // your code here if you want to use the results ! 
-//logReq.end(JSON.stringify(body))
-//str;
-//callback(str);
+var logReq = HTTPS.request(url, function(res) { 
+console.log(' Status: ' + res.statusMessage + ' Status code: ' + res.statusCode)
+var json = JSON.stringify(body);
+      //if (res.statusCode == 200) || (res.statusCode == 202) {
+        //neat
+//} else {
+        //console.log('rejecting bad status code ' + res.statusCode);
+      //}
+  });
 
 
-//logReq.on('error', function(err) { console.log('error posting message ' + JSON.stringify(err)); }); logReq.on('timeout', function(err) { console.log('timeout posting message ' + JSON.stringify(err)); });
 
-//logReq.end(JSON.stringify(body));
+
+
+  logReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  logReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+
+
+  logReq.end(JSON.stringify(body));
+
 
 }); 
 } 
 
 var req = HTTPS.request(options, callback).end();
+
+/*
+    options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+
+    body = {
+    
+    "attachments" : "attachments",
+    "bot_id"      : logID, 
+    "text"        : msg
+  };
+
+*/
+
+
+  console.log('sending response to text object ' + '\n' + msg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
