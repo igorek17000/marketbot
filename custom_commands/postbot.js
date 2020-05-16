3954076ca94538a89c04dba933b6c5b032c3a27a
dstlmike <dstl_mike1@hotmail.com>
2020-05-16 01:01:42 -0400

    Update postbot.js

--- custom_commands/postbot.js
+++ custom_commands/postbot.js
@@ -6,13 +6,13 @@ exports.modName = "Post module";
 
 logID = "b6c42cc2a1bee3c38f07723d78";
 var str = '';
-var response = "";
+//var response = "";
 
 
 
 var msg;
-msg = response;
-msg : msg;
+//msg = response;
+msg : str;
 
 
 options = {
@@ -35,7 +35,7 @@ body = {
     "text"        : msg
   };
 
-var json = JSON.stringify(body);
+var json = JSON.stringify(body));
 
 
 
