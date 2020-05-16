dfaee1d52ca311089aea4efcb69227238f4ea4bb
dstlmike <dstl_mike1@hotmail.com>
2020-05-15 20:42:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -240,7 +240,7 @@ var nickName = '';
 
 botReq = HTTPS.request(options, function(res) { 
 //console.log('\x1b[31m%s\x1b[0m', 'Hi' + ' Status: ' + res.statusMessage + ' Status code: ' + res.statusCode + '\n' + botResponse)
-//console.log(colors.red('%s'), nickName)
+//console.log(colors.red({'%s'}), nickName)
 var chalk = require('chalk'); 
 chalk.enabled = true; 
 
