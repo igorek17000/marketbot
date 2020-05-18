dd18d1a6d52e2526f26e5b458da34dba365cbe71
dstlmike <dstl_mike1@hotmail.com>
2020-05-18 16:52:12 -0400

    Update postbot.js

--- custom_commands/postbot.js
+++ custom_commands/postbot.js
@@ -62,7 +62,7 @@ response.on('data', function (chunk) {
 str += chunk; 
 }); 
 response.on('end', function () { 
-console.log(req.data); 
+//console.log(req.data); 
 console.log(str); // your code here if you want to use the results !
 });
 
