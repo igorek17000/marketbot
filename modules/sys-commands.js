ba36e75dfb51745ce650dcb505bbeebc0cf4db65
dstlmike <dstl_mike1@hotmail.com>
2020-05-20 03:17:21 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -844,7 +844,7 @@ direction: direction;
 
 
 var message = `It's ${gm.response.name}'; degrees in ${gm.response.name}!`;
-//var name = "name";
+var name = "name";
 var group_id = "group_id";
 var result = [];
 var msg;
@@ -868,7 +868,7 @@ result.sort(function(a, b) {
     //if (!cmdArray[cmd].desc)
      // cmdArray[cmd].desc = "No description provided ... thanks lazy mods";
 
-    if (result[name].group_id)
+    if (result[name])
       msg = result[name].name + result[name].group_id; //ownerArr.push(cmdArray[cmd]);
    /* else if (cmdArray[cmd].mod)
       modArr.push(cmdArray[cmd]);
