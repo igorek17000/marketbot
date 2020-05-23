0d15c7868ebf32df770fec525be800958f57a280
dstlmike <dstl_mike1@hotmail.com>
2020-05-23 02:45:19 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -803,8 +803,8 @@ console.log(names)
 
   
 
-var str = '';
-str = body;
+var str = [];
+str = response.body;
 var json = JSON.stringify(str); // response.result; // + " name at " + response.body.group_id; // + "\n" + "Currently: " + weather.main.temp + "° celsius" + "\n" + "Mainly: " + weather.weather[0].description + "\n" + "High: " + weather.main.temp_max +  "° celcius" + "\n" + "Low: " + weather.main.temp_min + "° celsius" + "\n" + "Relative humidity: " + weather.main.humidity + "\n" + "Wind: " + weather.wind.speed + " km/h" + "\n" + "Wind direction: Blowing from the " + direction + " bearing " + weather.wind.deg + "° degrees.";
 msg = str;
 //=======
