f7964b3285893c0728a607883f5fda58c54d314f
dstlmike <dstl_mike1@hotmail.com>
2020-05-24 01:08:17 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -811,7 +811,7 @@ console.log(names)
   var obj = JSON.parse(body); 
 var keys = Object.keys(obj.response); 
 for (var i = 0; i < keys.length; i++) { 
-console.log(obj[0][keys[i]]); 
+console.log(obj[keys[i]]); 
 console.log('-----------------' + '\n' + '\n');
 
 
