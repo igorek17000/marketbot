0effd8b07157cadfc4d6ee0c8bda31c22650f374
dstlmike <dstl_mike1@hotmail.com>
2020-06-25 19:55:18 -0400

    Update flynn-bot-timesheet.js

--- custom_commands/flynn-bot-timesheet.js
+++ custom_commands/flynn-bot-timesheet.js
@@ -78,20 +78,20 @@ exports.checkCommands = function(dataHash, callback) {
 
 
 
-     var flynnbReg = new RegExp(flynnb.regex, "i"); 
-     var flynnbcReg = new RegExp(flynnb.regexcurrent, "i");  
+     var flynnbReg = new RegExp(flynnb.regex, "i" || flynnb.regexcurrent, "i"); 
+    // var flynnbcReg = new RegExp(flynnb.regexcurrent, "i");  
         
 
-      if (flynnb.regexcurrent) // == "^\/" + "current" + "$") 
-      var flynnbcReg = new RegExp(flynnb.regexcurrent, "i");
+     // if (flynnb.regexcurrent) // == "^\/" + "current" + "$") 
+     // var flynnbcReg = new RegExp(flynnb.regexcurrent, "i");
 
 
      
       if (flynnb.bots.indexOf(dataHash.currentBot.type) > -1 && dataHash.request.text && flynnbReg.test(dataHash.request.text)){
         var val = flynnbReg.exec(dataHash.request.text);
       
-      if (flynnb.bots.indexOf(dataHash.currentBot.type) > -1 && dataHash.request.text && flynnbcReg.test(dataHash.request.text)){
-        var val = flynnbcReg.exec(dataHash.request.text);
+     // if (flynnb.bots.indexOf(dataHash.currentBot.type) > -1 && dataHash.request.text && flynnbcReg.test(dataHash.request.text)){
+       // var val = flynnbcReg.exec(dataHash.request.text);
    
 
      // if (flynnb.bots.indexOf(dataHash.currentBot.type) > -1 && dataHash.request.text && flynnbcReg.test(dataHash.request.text)){
@@ -123,7 +123,7 @@ exports.checkCommands = function(dataHash, callback) {
     break;
     }
   }
-}
+
 
 
   for (cmd in flynnBotCommands) {
