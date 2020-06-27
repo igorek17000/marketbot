2e0e7cf238dbf0c0dbafd8aa0318dcafaf0deaa9
dstlmike <dstl_mike1@hotmail.com>
2020-06-26 21:52:09 -0400

    Update flynn-bot-timesheet.js

--- custom_commands/flynn-bot-timesheet.js
+++ custom_commands/flynn-bot-timesheet.js
@@ -117,7 +117,8 @@ exports.checkCommands = function(dataHash, callback) {
     } else {
       msg = "Sunday\n" + flynnb.sunday + "\n" + "Monday\n" + flynnb.monday + "\n" + "Tuesday\n" + flynnb.tuesday + "\n" + "Wednesday\n" + flynnb.wednesday + "\n" + "Thursday\n" + flynnb.thursday + "\n" + "Friday\n" + flynnb.friday + "\n" + "Saturday\n" + flynnb.saturday;
     }
-         callback(true, "FlynnBot Timecard for " + flynnb.name + "\n" + msg, flynnb.attachments); // []);
+        var intro = "FlynnBot Timecard for " + flynnb.name + "\n";
+         callback(true, intro + msg, []); //"FlynnBot Timecard for " + flynnb.name + "\n" + msg, flynnb.attachments); // []);
     
 
     break;
