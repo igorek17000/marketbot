c90042c173d67752be69f8dc959a86d400cf1c86
dstlmike <dstl_mike1@hotmail.com>
2020-06-27 20:20:34 -0400

    Update flynn-bot-timesheet.js

--- custom_commands/flynn-bot-timesheet.js
+++ custom_commands/flynn-bot-timesheet.js
@@ -84,8 +84,8 @@ exports.checkCommands = function(dataHash, callback) {
 //if (flynnbReg.test(dataHash.request.text) || flynnbcReg.test(dataHash.request.text))
 //continue;
       if (flynnb.current == "current") {
-      flynnb.regex == flynnb.regex && flynnb.regexcurrent == flynnb.regex; //var flynnbcReg = new RegExp(flynnb.regexcurrent, "i");
-  }
+      flynnb.regex == flynnb.regex || flynnb.regexcurrent == flynnb.regex; //var flynnbcReg = new RegExp(flynnb.regexcurrent, "i");
+  
 
      // // if (flynnb.regex(dataHash.request.text))
 
@@ -127,7 +127,7 @@ exports.checkCommands = function(dataHash, callback) {
     break;
     }
   }
-//}
+}
 
 
   for (cmd in flynnBotCommands) {
