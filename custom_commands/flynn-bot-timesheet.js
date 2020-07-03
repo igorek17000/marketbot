2d063bea79b35c51f280cf07e66b6bf9bd309fd2
dstlmike <dstl_mike1@hotmail.com>
2020-07-02 21:26:33 -0400

    Update flynn-bot-timesheet.js

--- custom_commands/flynn-bot-timesheet.js
+++ custom_commands/flynn-bot-timesheet.js
@@ -43,8 +43,8 @@ db.updateOneDoc(db_table, { "regexcurrent": flynnb.regexcurrent }, { $set: { "re
 }
 
 function updateFlynnBotRegexCurrent(flynnb, callback) {
-    db.updateOneDoc(db_table, { "regexcurrent": flynnb.current }, { $rename: { "regexcurrent": "completed" }}, callback);
-db.updateOneDoc(db_table, { "current": flynnb.regexcurrent }, { $rename: { "current": "lastUpdated" }}, callback);
+    db.updateOneDoc(db_table, { "regexcurrent": flynnb.regexcurrent }, { $rename: { "regexcurrent": "completed" }}, callback);
+db.updateOneDoc(db_table, { "current": flynnb.current }, { $rename: { "current": "lastUpdated" }}, callback);
 
 }
 
@@ -214,7 +214,7 @@ function addFlynnBotCmd(request, bots, isMod, callback) {
 //for (flynnb in flynnbot) {   
       if (flynnbot[flynnb].current || flynnbot[flynnb].regexcurrent) {
        updateFlynnBotRegexCurrent(flynnbot[flynnb]);
-        updateFlynnBotRegexCurrent(flynnbot[flynnb]);
+       
         //var msg = "Current week updated";
         //callback(true, msg, []);
         }
