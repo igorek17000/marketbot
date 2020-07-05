7f626f3b8a4d8dcdbe0360d85e983b0d4bd8b9d8
dstlmike <dstl_mike1@hotmail.com>
2020-07-05 00:53:40 -0400

    Update flynn-bot-timesheet.js

--- custom_commands/flynn-bot-timesheet.js
+++ custom_commands/flynn-bot-timesheet.js
@@ -217,7 +217,7 @@ function addFlynnBotCmd(request, bots, isMod, currentBot, callback) {
     
       if (flynnbot[flynnb].current || flynnbot[flynnb].regexcurrent) {
 var botII = rooms.getRoom(botRoom);
-flynnbot[flynnb]["regexcurrent"] = botII;
+flynnbot[flynnb]["regexcurrent"] = rooms.getRoom(botRoom);
         updateFlynnBotCurrentDate(flynnbot[flynnb]);
         updateFlynnBotRegexCurrentDate(flynnbot[flynnb]);
      updateFlynnBotCurrent(flynnbot[flynnb]);
