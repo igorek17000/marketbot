4adcfa66c77b8ac507b2ec0ab25d641403145889
dstlmike <dstl_mike1@hotmail.com>
2020-07-08 01:40:10 -0400

    Update rooms.js

--- modules/rooms.js
+++ modules/rooms.js
@@ -57,7 +57,7 @@ var rooms = [];
 //if (rooms[path]) { 
 //room.name = res[room].id;
 room.type = res; 
-room.id = path[0]; 
+room.id = path.room; 
 room.one = rooms[path];
 //} 
 //if (botID == rooms[res[room].id]) { 
