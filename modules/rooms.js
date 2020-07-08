5c0822be5671940292f288a7605b0052c8f71236
dstlmike <dstl_mike1@hotmail.com>
2020-07-07 23:25:44 -0400

    Update rooms.js

--- modules/rooms.js
+++ modules/rooms.js
@@ -54,7 +54,7 @@ return room;
 exports.getARoom = function(path) {
 var room = {}; 
 //if (rooms[path]) { 
-room.name = Object.keys(path);
+room.name = res[room].name
 room.type = path; 
 room.id = rooms[path]; 
 //} 
