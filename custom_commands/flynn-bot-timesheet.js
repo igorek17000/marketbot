0e5e19bf8aa612e94821c8568cef1ebdabdf9b84
dstlmike <dstl_mike1@hotmail.com>
2020-07-12 03:02:51 -0400

    Update flynn-bot-timesheet.js

--- custom_commands/flynn-bot-timesheet.js
+++ custom_commands/flynn-bot-timesheet.js
@@ -258,7 +258,6 @@ flynnbot[flynnb]["regexcurrent"] = botName; //rooms.getUserPath(); //botName; //
       description: "Timesheet week of " + val[1],
       bots: Object.keys(bots),
       bot: currentBot,
-      bb: rooms.getRoom(),
       current: "current",
       date: date
      };
