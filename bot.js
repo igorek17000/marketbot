e8b73713bdcf54b93c6c37096c8682238ed45536
dstlmike <dstl_mike1@hotmail.com>
2020-07-13 00:49:15 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,7 +92,7 @@ var botRoom;
     currentBot:   rooms.getRoom(botRoom),
     isMod:        mods.isMod(request.user_id),
     bots:         rooms.getRooms(),
-    botARoom:     rooms.getUserPath(botRoom),
+    botARoom:     rooms.getUserPath(botRoom.type),
     funMode:      sysCommands.fun_mode(),
     owner:        config.env().owner
   };
