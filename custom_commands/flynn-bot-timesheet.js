c582b4af9b4d3cf5bf8ec0512908559dfe33936f
dstlmike <dstl_mike1@hotmail.com>
2020-07-15 19:27:05 -0400

    Update flynn-bot-timesheet.js

--- custom_commands/flynn-bot-timesheet.js
+++ custom_commands/flynn-bot-timesheet.js
@@ -198,7 +198,7 @@ exports.getCmdListDescription = function () {
   return null;
 }
 
-function addFlynnBotCmd(request, bots, isMod, currentBot, callback) {
+function addFlynnBotCmd(request, bots, isMod, currentBot, botARoom, callback) {
   var regex = /^\/timesheet add (.+?) ([\s\S]+)/i;
   var reqText = request.text;
  // var path = path || 'path' || Path || PATH || 'Path' || 'PATH' || path.toLowerCase();
