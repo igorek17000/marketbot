df396e54323de5758bf17d72c22489dc0b479d67
dstlmike <dstl_mike1@hotmail.com>
2020-07-18 00:15:18 -0400

    Update flynn-bot-timesheet.js

--- custom_commands/flynn-bot-timesheet.js
+++ custom_commands/flynn-bot-timesheet.js
@@ -231,7 +231,7 @@ function addFlynnBotCmd(request, bots, isMod, currentBot, botARoom, callback) {
 if (flynnbot[flynnb].current || flynnbot[flynnb].regexcurrent) {
 
 //if (flynnbot["rooms"].name == rooms.getRoom(room)) { // botRoom == '308boonbot') {
-botName = Object.values(botARoom); //rooms.getUserPath(botRoom);
+botName = Array(botARoom); //rooms.getUserPath(botRoom);
 
 //if (rooms.getRoom().room) //&& botRoom == '308boonbot') { //flynnbot[flynnb].bots.indexOf(getARoom.type) > -1 && botRoom == 'true') {
 //botName = Object.entries(currentBot) = [ ["name": botRoom.botName], ["type": botRoom.type], ["id": botRoom.id] ];
