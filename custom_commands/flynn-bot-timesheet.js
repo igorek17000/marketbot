33add00fb68f8584e33cbdef7041584d04a28a26
dstlmike <dstl_mike1@hotmail.com>
2020-07-20 01:52:36 -0400

    Update flynn-bot-timesheet.js

--- custom_commands/flynn-bot-timesheet.js
+++ custom_commands/flynn-bot-timesheet.js
@@ -296,7 +296,7 @@ function describeFlynnBotCmd(request, bots, isMod, currentBot, botARoom, callbac
         flynnbot[flynnb]["description"] = val[2];
       if (flynnbot[flynnb].description) {
         var des = val[2];
-        flynnbot[flynnb]["description"] = description.push(des);
+        flynnbot[flynnb]["description"] = flynnb.description.push(des);
         updateFlynnBotDesc(flynnbot[flynnb]);
         var msg = "FlynnBot timesheet description updated for " + flynnbot[flynnb].name;
 
