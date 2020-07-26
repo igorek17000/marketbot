340ded7aa9d4a27ca394048878141ce51f941ebf
dstlmike <dstl_mike1@hotmail.com>
2020-07-25 23:39:42 -0400

    Update exp.js

--- modules/exp.js
+++ modules/exp.js
@@ -1,6 +1,6 @@
 // Helper module
-var moment = require('moment-timezone'); 
-var date = moment.tz('America/Toronto').format('LLLL');
+//var moment = require('moment-timezone'); 
+//var date = moment.tz('America/Toronto').format('LLLL');
 
 function dated() {
 var moment = require('moment-timezone'); 
