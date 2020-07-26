d5450521a140e33d9428544a22811a07d5beaa56
dstlmike <dstl_mike1@hotmail.com>
2020-07-25 20:55:46 -0400

    Update exp.js

--- modules/exp.js
+++ modules/exp.js
@@ -3,9 +3,7 @@ var moment = require('moment-timezone');
 var date = moment.tz('America/Toronto').format('LLLL');
 
 
-exports.alexbotDate = function() {
-return date;
-}
+exports.alexbotDate = moment.tz('America/Toronto').format('LLLL');
 
 
 
