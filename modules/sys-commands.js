cbc8799a8054d1fd540d9c82a313463622872f68
dstlmike <dstl_mike1@hotmail.com>
2020-07-26 00:02:52 -0400

    Update sys-commands.js

--- modules/sys-commands.js
+++ modules/sys-commands.js
@@ -3,6 +3,7 @@
 var fun_mode = true;
 var sysCommands = [dateCmd, weatherCurrentCmd, weatherForecastCmd, funCmd, noFunCmd, idCmd, aboutCmd, eemailCmd, helpCmd, hiCmd, listCmd, nest18Cmd, nest19Cmd, nest20Cmd, nest21Cmd, nest22Cmd, nowCmd, gmCmd];
 var exp = require('./exp.js');
+var nes = require('./modules/nest18.js');
 
 var { stdin, stdout } = process;
 //var weather = require('./toronto');
@@ -545,9 +546,9 @@ if (regex.test(dataHash.request.text)) {
 
     
     callback(true, "Thermostat set to 18 degrees celsius", []);
-
-  var nest18 = require('./modules/nest18');
-nest18;
+nes.nest18;
+ // var nest18 = require('./modules/nest18');
+//nest18;
 } else {
 callback(true, "Access Denied! Only mods can adjust the temperature", []);
 }
