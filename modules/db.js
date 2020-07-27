f613c0863016996de376db216a1af0bb73dcea92
dstlmike <dstl_mike1@hotmail.com>
2020-07-27 02:36:40 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -80,7 +80,7 @@ exports.addDoc = function(collection, doc, callback) {
 exports.moveOneDoc = function(collection, matchHash, callback) {
 connect(function(db){
 var cursor = db.collection(collection).find(matchHash).each(function(err, doc){ 
-var ret = db.collection(collection).insert(doc, function(err, result);
+var ret = db.collection(collection).insert(doc, function(err, result){
 cursor;
 if (doc != null)
 ret.push(doc);
