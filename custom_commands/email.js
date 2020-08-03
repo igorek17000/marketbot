b574718eafbcaabd83ed41b64e9e59b2aacb452e
dstlmike <dstl_mike1@hotmail.com>
2020-08-03 02:57:12 -0400

    Update email.js

--- custom_commands/email.js
+++ custom_commands/email.js
@@ -63,9 +63,9 @@ function updateDraftt(cmd, callback) {
 }
 
 function updateUndraft(cmd, callback) {
-//setTimeout(function() {
+setTimeout(function() {
   db.updateOneDoc(db_table, {"status": cmd.status}, {$set: { "status": "archived"}}, callback);
-//}, config.delay_four);
+}, config.delay_one);
 }
 
 function updateFlynnBotCurrent(flynnb, callback) {
@@ -80,7 +80,7 @@ function updateSent(cmd, callback) {
 setTimeout(function() {
 
   db.updateOneDoc(db_email_sent, {"status": cmd.status}, {$set: { "status": "sent " + date }}, callback);
-}, config.delay_four);
+}, config.delay_one);
 }
 
 function updateSubject(cmd, callback) {
@@ -94,13 +94,13 @@ function updateBody(cmd, callback) {
 function moveSentDoc(cmd, callback) {
 setTimeout(function() {
 db.moveOneDoc(db_email_sent, cmd, callback);
-}, config.delay_one);
+}, config.delay_two);
 }
 
 function moveArchiveDoc(cmd, callback) {
 setTimeout(function() {
 db.moveOneDoc(db_email_archived, cmd, callback);
-}, config.delay_one);
+}, config.delay_two);
 }
 
 
@@ -108,7 +108,7 @@ function deleteDoc(cmd, callback){
   var findJson = { "status": cmd["status"] };
 setTimeout(function() {
   db.removeOneDoc(db_table, findJson);
-}, config.delay_two);
+}, config.delay_three);
 }
 
 
