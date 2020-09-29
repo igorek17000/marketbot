957c383dbb8b95009e81afd6627a80c220916bbe
dstlmike <dstl_mike1@hotmail.com>
2020-09-29 01:39:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ http        = require('http');
 director    = require('director');
 bot         = require('./bot.js');
 today       = require('./bot.js');
-var down = require('./modules/countdown-list.js');
+//var down = require('./modules/countdown-list.js');
 
 //image       = require('./2A34A9R.jpg');
 
