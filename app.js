8f02687216c37a91f2bf9767c68dcecede1bbd25
dstlmike <dstl_mike1@hotmail.com>
2020-10-25 14:07:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha
 //}
 
 //var connection_string = mongodb+srv://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-esmha.mongodb.net/test?retryWrites=true&w=majority
-
+/*
 if(process.env.MONGODB_PASSWORD){
   // //connection_string = 'mongodb://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority' + process.env.MONGODB_USER + ":" +
   connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority' + process.env.MONGODB_USER + ":" +
@@ -33,7 +33,7 @@ if(process.env.MONGODB_PASSWORD){
   process.env.MONGODB_SERVICE_PORT + '/' +
   process.env.MONGODB_DATABASE;
 }
-
+*/
 
 var express = require('express'); 
 
