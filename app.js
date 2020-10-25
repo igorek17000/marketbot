dfc995c83d3b3cf599b9866f8c9497a40cca6159
dstlmike <dstl_mike1@hotmail.com>
2020-10-25 15:01:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var mongoDB     = require('mongodb').MongoClient;
 
 
 //var db = require('mongodb').Db;
-var bot = require('../bot.js');
+var bot = require('./bot.js');
 //var connection_string = 'mongodb://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 //var connection_string = 'mongodb://0.0.0.0:27017/test';
