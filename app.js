780f2d6cace6d5c55294e3feb251f0aebe8da249
dstlmike <dstl_mike1@hotmail.com>
2020-10-26 03:37:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var mongoDB     = require('mongodb').MongoClient;
 //var mongoose = require('mongodb').MongoClient;
 
 
-var db = require('mongodb').Db;
+//var db = require('mongodb').Db;
 var bot = require('./bot.js');
 //var connection_string = 'mongodb://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 //var connection_string = 'mongodb://0.0.0.0:27017/test';
-//var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
-var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017/sampledb';
+//var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017/sampledb';
 
 //var connection_string = 'mongodb+srv://dstlmike1:308boonave@cluster0-esmha.mongodb.net/test';
 //var connection_string = 'mongodb://0.0.0.0:27017/sampledb';
@@ -57,17 +57,17 @@ callback(db);
 */
 //mongoose.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority');
 
-//mongoose.connect(connection_string);
-//function connect(callback){ 
+mongoose.connect(connection_string);
+function connect(callback){ 
 //var db = require(./modules/db.js');
-//var db = mongoose.connection; //('mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'); 
+var db = mongoose.connection; //('mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'); 
 
-//db.on('error', console.log.bind(console, "connection error")); 
+db.on('error', console.log.bind(console, "connection error")); 
 
-//db.once('open', function(callback){ 
+db.once('open', function(callback){ 
 
-   // console.log("connection succeeded"); 
-//});
+    console.log("connection succeeded"); 
+});
 
   /*
 var MongoClient = require('mongodb').MongoClient;
