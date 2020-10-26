6b8c5cf7f1336ae568de09ba4e57d9b4a7fc325c
dstlmike <dstl_mike1@hotmail.com>
2020-10-26 03:35:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ db.collection('details').insertOne(data,function(err, collection){
           
 
     return res.redirect('signup_success.html'); 
-}) 
+});
 
   
 
@@ -147,7 +147,7 @@ res.set({
     }); 
 
 return res.redirect('index.html'); 
-}).listen(port);
+});
 var port = 8080 || 3002 || 27017 || 27016 || 27015 || 5858;
 //var port = Number(process.env.NODEJS_SERVICE_PORT || process.env.PORT || 8080 || 3002); 
 var ip = process.env.NODEJS_SERVICE_IP || "0.0.0.0" || "127.0.0.1"; 
