9c0c72594af70e518c712356f4c9f7f1a5df0b91
dstlmike <dstl_mike1@hotmail.com>
2020-10-26 04:05:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ var db = mongoose.connection; //('mongodb://alexbot:308boonave@cluster0-shard-00
 
 db.on('error', console.log.bind(console, "connection error")); 
 
-db.once('open', function(callback){ 
+db.on('data', function(callback){ 
 
     console.log("connection succeeded"); 
 });
