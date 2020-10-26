94a627b50f484cea1b74d35db1d1d6a454cc154e
dstlmike <dstl_mike1@hotmail.com>
2020-10-26 00:37:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ mongoose.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongod
 
 //mongoose.connect(connection_string);
 
-var db = mongoose.connect; 
+var db = mongoose.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'); 
 
 db.on('error', console.log.bind(console, "connection error")); 
 
