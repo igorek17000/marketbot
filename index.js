9003a4b24ea7fe058d98c17ed691ef3e6b6eacb7
dstlmike <dstl_mike1@hotmail.com>
2020-10-26 08:01:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ this.res.write(html);
 
 function rend() {
   this.res.statusCode = 200; 
-this.res.setHeader('Content-type', 'application/json'); 
+this.res.setHeader('Content-type', 'text/html'); 
 var html = fs.readFileSync(path.join(__dirname + "/app.js")); 
 this.res.write(html); 
 this.res.end();
