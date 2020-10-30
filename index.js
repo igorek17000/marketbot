5dd39c7027acf9af65467f21110d2a2221a10ef1
dstlmike <dstl_mike1@hotmail.com>
2020-10-29 21:29:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -294,12 +294,12 @@ var name = this.req.body.name;
      // var name = this.req.params.name; 
     //  var value = this.req.params.value; 
 //collection.findOne({[name]:value}, function(err, item) { res.send(item); });
-     var doc = naame;
-var naame = doc;
+    // var doc = doc;
+var naame = "doc.name";
    var cursor = db.collection('details').find({name:naame}); 
 var ret = []; 
 cursor.each(function(err, doc){
-var naame = doc.name;
+var naame = "doc.name";
   //if(doc.name = name) {
 if(doc.name == data.name) {
 //if(name != doc.name) {
