82fd471f7e04f6a404dfb981428ceea6aa053692
dstlmike <dstl_mike1@hotmail.com>
2020-11-01 15:15:57 -0500

    Update mongodbbot.js

--- mongodbbot.js
+++ mongodbbot.js
@@ -10,7 +10,7 @@ var app = express()
 var path = require('path');
 var bodyParser = require("body-parser"); 
 var mongoose = require('mongoose'); 
-var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'; 
+var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'; 
 var db = mongoose.connection; 
 var nodemailer = require('nodemailer');
 var moment = require('moment'); 
