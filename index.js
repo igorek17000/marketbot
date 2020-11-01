e40b1d41e8b1bf66ad2be61b7b174d3a6f7fac85
dstlmike <dstl_mike1@hotmail.com>
2020-11-01 15:58:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -311,7 +311,7 @@ results.each(function(err, doc) {
 //results.each(); //(res, i) => { 
 //i = 0;
 //if (results.doc > 0) { //> 0) {
-if (name = 1) { //> 0) { //= name) { // = data.name) {
+if (name) { //> 0) { //= name) { // = data.name) {
 //--//--// cursor.each(function(err, doc){
 
 doc = {
