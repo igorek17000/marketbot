82f04377f4e5777a967e2f243add52a018207a52
dstlmike <dstl_mike1@hotmail.com>
2020-11-04 00:34:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -322,7 +322,7 @@ function iterateFunc(doc) {
 }
 */
 console.log(JSON.stringify(doc, null, 4)); 
-if (!data.name) { //name != data.name) { //true) { //.name != data.name) { //doc["name"] != data.name) { //!= 1 || doc != 2 || doc != 3 || doc != 4) {
+if ("data.name" != null) { //name != data.name) { //true) { //.name != data.name) { //doc["name"] != data.name) { //!= 1 || doc != 2 || doc != 3 || doc != 4) {
 addit();
 //console.log(data.name + " already exist");
 
