9133e1689cf438c19d7f2e7573e260d8366bfdc9
dstlmike <dstl_mike1@hotmail.com>
2020-11-05 22:30:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -307,12 +307,14 @@ var res = {
 }
 */
 function addit() {
+if (!doc.name) {
 db.collection('details').insertOne(data, function(err, collection){ 
-        if (!err) 
-// throw err; 
+        if (err) 
+throw err; 
 console.log(data.name + "\n User added"); 
 //console.log(result + "\n added"); 
 });
+}
 }
 
 function iterateFunc(doc) {
