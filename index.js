005bf90f40027100aff84d73ba554bab66bed970
dstlmike <dstl_mike1@hotmail.com>
2020-11-07 17:45:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -307,7 +307,7 @@ function getAllDocs() {
 
 db.collection('details').find({name}).toArray(function(err, docs) { 
 if(err) throw err; 
-if (docs !< 1) {
+if (docs < -1) {
 console.log(docs); //db.close(); 
 }
 if (docs < 1) {
