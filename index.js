13908dec1cbfc1132cc7cf98f77ac1bd1a629e99
dstlmike <dstl_mike1@hotmail.com>
2020-11-09 22:35:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -410,7 +410,7 @@ getAllDocs();
 
   this.res.statusCode = 200;
 this.res.setHeader('content-type', 'text/html', 'Access-control-Allow-Origin', '*');
-var html = fs.readFileSync(path.join(__dirname + "/commands_success/command_success.html"));
+var html = fs.readFileSync(path.join(__dirname + "/commands_success/command.html"));
 //trw();
 this.res.write(html);
 //this.res.redirect('signup_success.html');
