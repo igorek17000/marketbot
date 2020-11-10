1671a75aed0cadbee0cea1c0414a43d46c71e18f
dstlmike <dstl_mike1@hotmail.com>
2020-11-10 02:19:46 -0500

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -57,7 +57,7 @@ exports.getSuccessDocuments = function(collection, callback) {
   mongoDB.connect(connection_string, function(err, db) {
 
     if(err) throw err;
-   var allDocs = db.collection('details').find({"name": name});
+   var allDocs = db.collection('details').find({"name": docs[name]});
 allDocs.toArray(function(err, docs) {
       callback(docs);
       //db.close();
