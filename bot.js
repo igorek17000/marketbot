e544f50b098fa0c2f810728d38af60728519c182
dstlmike <dstl_mike1@hotmail.com>
2020-11-09 23:35:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -268,7 +268,7 @@ console.log(error);
 
 getAllDocs();
 
-
+/*
   this.res.statusCode = 200;
 this.res.setHeader('content-type', 'text/html', 'Access-control-Allow-Origin', '*');
 //var html = fs.readFileSync(path.join(__dirname + "/commands_success/command_success.html"));
@@ -276,10 +276,10 @@ this.res.setHeader('content-type', 'text/html', 'Access-control-Allow-Origin', '
 this.res.write();
 //this.res.redirect('signup_success.html');
 this.res.end(outputSuccess);
-/*
+*/
 this.res.writeHead(200, {"Content-Type": "text/html"}); 
 this.res.end(outputSuccess);
-*/
+
 }
 
 
