46fb5bc3ff1de18586954b3e5081a2043d301366
dstlmike <dstl_mike1@hotmail.com>
2020-11-12 20:39:43 -0500

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -68,7 +68,7 @@ exports.getSuccessDocuments = function(collection, matchHash, callback) {
         //throw err;
         //var html = fs.readFileSync(path.join(__dirname + "/signup_success.html")); 
       //}
-      if (matchHash) { // != null) { //callback) {
+      if (docs["data.name"]) { // != null) { //callback) {
         //callback(docs);
         console.log(docs)
         //cmdit();
