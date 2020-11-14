c40ca8fa4ff060c5d7e1f6b524797a6c72a14824
dstlmike <dstl_mike1@hotmail.com>
2020-11-14 03:05:52 -0500

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -52,7 +52,7 @@ exports.getAllDocuments = function(collection, callback) {
 }
 
 //---- Success
-exports.getSuccessDocument = function(collection, matchHash, doc, callback) {
+exports.getSuccessDocument = function(collection, matchHash, docs, callback) {
   mongoDB.connect(connection_string, function(err, db) {
 
     if(err) throw err;
