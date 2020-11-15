0570e5f70a7167b1db5f2595bdd70b0bae5e5069
dstlmike <dstl_mike1@hotmail.com>
2020-11-15 18:52:22 -0500

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -96,7 +96,7 @@ console.log(data.name + "\n User added");
 }
 
 function iterateFunc() {
-results.toArray(function(err, docs) { //, errorFunc);
+db.collection('details').find({matchHash}).toArray(function(err, docs) { //, errorFunc);
 
 if (docs < 1) {
 additFunc();
