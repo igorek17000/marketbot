e84da6dc470790a4988b65e0f58c1c6ec5c14d4e
dstlmike <dstl_mike1@hotmail.com>
2020-11-17 22:45:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -413,12 +413,12 @@ getAllDocs();
 this.res.setHeader('content-type', 'text/html', 'Access-control-Allow-Origin', '*');
 var html = fs.readFileSync(path.join(__dirname + "/commands_success/command_success.html"));
 //trw();
-//this.res.write(html);
+this.res.write(html);
 //this.res.redirect('signup_success.html');
-//this.res.end();
+this.res.end();
 
-this.res.writeHead(200, {"Content-Type": "text/html"}); 
-this.res.end(outputSuccess);
+//this.res.writeHead(200, {"Content-Type": "text/html"}); 
+//this.res.end(outputSuccess);
 
 }
 
