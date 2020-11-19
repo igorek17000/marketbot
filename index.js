7517cb3758966bc94170849fc88c4dbf78d8cb29
dstlmike <dstl_mike1@hotmail.com>
2020-11-18 22:23:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ moment.tz.setDefault('America/Toronto');
 //var date = moment().utcOffset(-300).format('LLLL');
 var date = moment().format('LLLL');
 var commandListSuccess = require('./commands_success/command-list.js'); //commandListSuccess.buildHTML(cmdArr, config.bot_name);
-var config = require('./config/config.js');
+//var config = require('./config/config.js');
 
 
 
@@ -298,7 +298,7 @@ this.res.end();
 
 
 function commands_success() {
-
+  var bot_name = "AlexBot";
   var cmdArr = [];
   //function cmdit() {
 //console.log('displaying commands at /commands');
@@ -310,7 +310,7 @@ function commands_success() {
   }
 //}
 */
- var outputSuccess = commandListSuccess.buildHTML(cmdArr, config.bot_name);
+ var outputSuccess = commandListSuccess.buildHTML(cmdArr, bot_name);
 
 
 var name = this.req.body.name;
