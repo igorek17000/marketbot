36aa8847dffb69a8da3399fae6685bc340985b4e
dstlmike <dstl_mike1@hotmail.com>
2020-12-01 23:37:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -323,7 +323,7 @@ var output = 'commandList.buildHTML(cmdArr, config.bot_name)';
 
 exports.commands = function() {
 var output = '';
-output = 'commandList.buildHTML(cmdArr, config.bot_name)';
+output = 'commandList.buildHTML(cmdArr, config.bot_name);';
 var docs, data;
 var name = this.req.body.name;
     var email = this.req.body.email;
