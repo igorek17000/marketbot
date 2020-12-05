f59ae747a88792cb5b3727e1d8476211fc038cc8
dstlmike <dstl_mike1@hotmail.com>
2020-12-05 02:29:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ if (callback)
 callback(output);
   //  console.log(output);
 */
-});
+//});
 
   
 //dbs.on('error', console.error.bind(console, 'connection error:')); var onErr = function(err,callback){ mongoose.connection.close(); callback(err);}; exports.teamlist = function(gname,callback){ db.once('open', function(){ var teamSchema = new mongoose.Schema({ country: String, GroupName: String }); var Team = db.model('Team', teamSchema) Team.find({'GroupName':gname}, function (err, teams) { if(err){ onErr(err,callback); }else{ mongoose.connection.close(); console.log(teams); callback("",teams); } })// end Team.find });// end db.once open};
