069609a5cb30cd1c9e7ba61e17f67e7faa678240
dstlmike <dstl_mike1@hotmail.com>
2020-12-06 23:13:14 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -443,10 +443,10 @@ var cmdarray = [];
 var output = commandList.buildHTML(cmdArr, config.bot_name);
 //getTheDocs;
 //console.log(output);
-/*
+
   this.res.writeHead(200, {"Content-Type": "text/html"});
   this.res.end(output);
-*/
+
 //}
 }
 
