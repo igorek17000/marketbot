a605b6772dff627dc48a96754454502fd9d72151
dstlmike <dstl_mike1@hotmail.com>
2020-12-07 21:00:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -387,7 +387,7 @@ var output = 'commandList.buildHTML(cmdArr, config.bot_name)';
 
 exports.commands = function() {
 var docs = [];
-var ret = dbs.collection('details').find({name}).exec();
+var ret = dbs.collection('details').find({name});
 mongoose.connect(connection_string, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //hard coded temporarily ... maybe permanently ... losing motivation to work on this //if(alexb.name == 'cc' && dataHash.currentBot.type == 'hp') //continue; var alexbReg = new RegExp(alexb.regex, "i"); if (dataHash.request.text && alexbReg.test(dataHash.request.text)){ var val = alexbReg.exec(dataHash.request.text); // if (dataHash.currentBot("282865de8ce30137567238148f")) { //var msg = "308BoonBot\n" + alexb.message; callback(true, alexb.message, alexb.attachments, []); break;
