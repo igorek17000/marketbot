9202c102c6e1292ec5ac3e027dac676a1259bb17
dstlmike <dstl_mike1@hotmail.com>
2020-12-09 13:47:40 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ dbs.on('error', console.log.bind(console, "connection error"));
 dbs.once('open', async function(callback){
 global.customerSchema = new mongoose.Schema({ name: String, email: String, pass: String, phone: String }); 
 global.Customer = mongoose.model('Customer', customerSchema, 'details'); 
-global.docs = await Customer.find({name}); 
+global.docs = await Customer.find({datta.name}); 
 
  /*
 if (callback)
@@ -377,7 +377,7 @@ var name = this.req.body.name;
     var phone = this.req.body.phone;
     //var out = output; //commandList.buildHTML(cmdArr, config.bot_name);
   
-
+global.datta = data;
     var data = {
         "name": name,
         "email": email,
