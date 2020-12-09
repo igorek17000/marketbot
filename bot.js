3a13edc3e032d19ebe2775f227195ddda3b3f404
dstlmike <dstl_mike1@hotmail.com>
2020-12-08 19:09:48 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -472,7 +472,7 @@ console.log(docs);
 
 } 
 });
-
+//const mongoose = require('mongoose'); run().catch(error => console.log(error.stack)); async function run() { await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true }); // Clear the database every time. This is for the sake of example only, // don't do this in prod :) await mongoose.connection.dropDatabase(); const customerSchema = new mongoose.Schema({ name: String, age: Number, email: String }); const Customer = mongoose.model('Customer', customerSchema); await Customer.create({ name: 'A', age: 30, email: 'a@foo.bar' }); await Customer.create({ name: 'B', age: 28, email: 'b@foo.bar' }); // Find all customers const docs = await Customer.find(); console.log(docs); }
 
 var mongoose = require('mongoose'); 
 run().catch(error => console.log(error.stack)); 
@@ -486,7 +486,7 @@ var Customer = mongoose.model('Customer', customerSchema);
 //await Customer.create({ name: 'A', age: 30, email: 'a@foo.bar' }); 
 //await Customer.create({ name: 'B', age: 28, email: 'b@foo.bar' }); 
 // Find all customers 
-var docs = await Customer.find(data.name); 
+var docs = await Customer.find(name); 
 console.log(docs); 
 }
 //run();
