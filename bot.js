12b7110c932273f9c3646d2437e3d9893cf48d69
dstlmike <dstl_mike1@hotmail.com>
2020-12-08 19:00:43 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -481,7 +481,7 @@ run().catch(error => console.log(error.stack));
 async function run() { 
 await mongoose.connect(connection_string, { useNewUrlParser: true, useUnifiedTopology: true });
 //await mongoose.connection.dropDatabase(); 
-var customerSchema = new mongoose.Schema({ name: string, email: string, pass: string, phone: string }); 
+var customerSchema = new mongoose.Schema({ name: String, email: String, pass: String, phone: String }); 
 var Customer = mongoose.model('Customer', customerSchema); 
 //await Customer.create({ name: 'A', age: 30, email: 'a@foo.bar' }); 
 //await Customer.create({ name: 'B', age: 28, email: 'b@foo.bar' }); 
