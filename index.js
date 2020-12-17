3f7ef38c8cde3595f970b6895056372974714423
dstlmike <dstl_mike1@hotmail.com>
2020-12-17 01:31:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -303,7 +303,7 @@ this.res.setHeader('content-type', 'text/html', 'Access-control-Allow-Origin', '
 */
 var dbh = db.collection('details').find({}, function(err, docs) { 
 dbh;
-if (err) this.res.statusCode = 400;
+if (err) this.res.statusCode(400);
 this.res.setHeader('content-type', 'text/html', 'Access-control-Allow-Origin', '*');
 
 
