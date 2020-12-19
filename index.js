a47cde9465d90bd9bbf1731a4268b8a3b1f5e3c2
dstlmike <dstl_mike1@hotmail.com>
2020-12-18 22:25:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -303,7 +303,7 @@ this.res.end();
 
 function clicks() {
 var ret = [];
-var name = db.collection('details').find({}, function(callback) { if (callback) { callback(ret); } });
+var name = db.collection('details').find({}).toArray(function(callback) { if (callback) { callback(ret); } });
   /*  var email = this.req.body.email;
     var pass = this.req.body.password;
     var phone = this.req.body.phone;
@@ -328,7 +328,7 @@ console.log(dbhs[{name}]);//this.res.send(result);
 if (name) {
 console.log(ret);
 }
-var html = this.res.IncomingMessage; //fs.readFileSync(path.join(__dirname + "/index.html"));
+var html = this.res[IncomingMessage]; //fs.readFileSync(path.join(__dirname + "/index.html"));
 this.res.write(html);
 this.res.end(); 
 //});
