d3dd02d1d91d329f7453fa815e3cfa88c6026791
dstlmike <dstl_mike1@hotmail.com>
2020-12-21 21:12:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ var bodyParser = require("body-parser");
 
 var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
-
+/*
 var mongoose = require('mongoose');
 
 mongoose.connect(connection_string, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -181,26 +181,33 @@ db.once('open', function(callback){
     console.log("connection succeeded");
 })
 
-  
+  */
 
 var app = express();
 
   
 
 
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-//app.use(express.static('public'));
-//app.use(bodyParser.urlencoded({
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
 
-   //extended: true
-//}));
+   extended: true
+}));
 
+app.listen(port, ip, function() {
+console.log('App Listening');
+}
+
+app.get('/hi', function(req, res) {
+res.send('Hello');
+}
 
 /*
 app.post('/signup_success', function(req,res){
-*/
-/*
+
+
     var name = doc.name; // this.req.name; //body.name;
 
     var email = doc.email; //this.req.email; //body.email;
