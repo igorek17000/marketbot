0bb49e9ba996a3956507b7f2996907feb7d99e88
dstlmike <dstl_mike1@hotmail.com>
2020-12-21 20:36:18 -0500

    Update ind.js

--- ind.js
+++ ind.js
@@ -1,1 +1,34 @@
 
+var express = require("express");
+
+var bodyParser = require("body-parser");
+
+var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+
+var mongoose = require('mongoose');
+
+mongoose.connect(connection_string, { useNewUrlParser: true, useUnifiedTopology: true });
+
+var db = mongoose.connection;
+
+db.on('error', console.log.bind(console, "connection error"));
+
+db.once('open', function(callback){
+    console.log("connection succeeded");
+})
+
+  
+
+var app = express();
+
+  
+
+
+//app.use(bodyParser.json());
+
+//app.use(express.static('public'));
+//app.use(bodyParser.urlencoded({
+
+   //extended: true
+//}));
