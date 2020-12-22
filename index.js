c1f6335b96778d8f274b4316622101dc9c9a5bb5
dstlmike <dstl_mike1@hotmail.com>
2020-12-21 21:35:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
 
    extended: true
