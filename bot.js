6abd2510b20d174b99b23ff8e771a6511cced65f
dstlmike <dstl_mike1@hotmail.com>
2020-12-27 18:46:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,7 +118,7 @@ exports.init = function() {
 exports.respond = function(botRoom) {
 //var botRoom = rooms.getUserPath(botRoom);
 //var chuncks = [];
-  var request = JSON.parse(this.req.chunks[0]);
+  var request = bodyParser.json(this.req.chuncks[0]); // JSON.parse(this.req.chunks[0]);
 
   var dataHash = {
 
