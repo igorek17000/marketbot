08fe68b79d698873b6ca3c4a0e72790772bf1ade
dstlmike <dstl_mike1@hotmail.com>
2021-01-01 05:23:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -119,12 +119,12 @@ function botRoom(req, res) { //botRoom, chunks, chunk) {
 //app.use('/bot/:botRoom', function(req, res) {
 var chunks = [];
 var chunk = '';
-req.chunks = [];
+req.chunks = IncomingMessage;
 //chunks = chunks += chunk;
 
 
 
-var request = req.chunks[0]; //req.param); 
+var request = req.chunks.chunks; //req.param); 
 //app.post('/bot/:botRoom', function(req, res) {
 //req.on('data', function(chunk) {
 //chunks = chunks += chunk;
