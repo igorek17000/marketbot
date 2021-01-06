9e042004468011e578d3ab05ed3a653566f70e73
dstlmike <dstl_mike1@hotmail.com>
2021-01-05 23:04:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ req.on('end', function(chunk) {
 //req.chunks = [];
 //chunk = '';
 var resi = req.chunks + ' 1'; //chunks += chunk;
-global.ressi = thus.res;
+global.ressi = this.res;
 //---var request = this.req; // chunks += chunk;
 //var o = 0; 
 //var a = 1;
