02390df79cefcc9fa98e77cdf28110705bbb01c1
dstlmike <dstl_mike1@hotmail.com>
2021-01-08 15:37:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -238,7 +238,7 @@ app.use('/bot/:botRoom', function(req, res) {
 //var request = this.req.chunks[0]; //chunks += chunk;
 //var chunks = [];
 req.chunks = [];
-req.chunckit = [];
+req.chunkit = [];
 res.chunks = []
 //var chunk = '';
 //var resi = chunks += chunk;
