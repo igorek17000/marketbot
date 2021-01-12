706c3a32dee1c6ac81242031e482312a872d7a55
dstlmike <dstl_mike1@hotmail.com>
2021-01-11 19:50:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ console.log(res);
  //--- });
 
 
-});
+//});
 
 /*
 http.createServer(appfunction (req, res) {
@@ -185,11 +185,11 @@ ip = process.env.NODEJS_SERVICE_IP || "0.0.0.0" || "127.0.0.1";
 //server.listen(port, ip);
 
 server; //(port, ip, function() {
-console.log('Server started at ' + date + ' & listening on port ' + port);
+//console.log('Server started at ' + date + ' & listening on port ' + port);
 //router.post('Server');
 
 //console.log((new Date()) + ' Server is listening on port ' + port);
-});
+//});
 
 //--------------
 
