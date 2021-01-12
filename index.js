d5a504e7914e8ef3c17b0e436026d9b32b229d51
dstlmike <dstl_mike1@hotmail.com>
2021-01-11 21:02:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -252,9 +252,9 @@ req.chunks.push(chunk.toString());
 }).listen(port);
 */
 
-app.listen('/', function() {
-console.log('App Listening');
-});
+app.listen('/'); //function() {
+//console.log('App Listening');
+//});
 
 var myBotRoom = function(req, res, next) {
 bot.respond();
