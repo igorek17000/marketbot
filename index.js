fafd669c978db53248f61e44be80fcdad06f7b92
dstlmike <dstl_mike1@hotmail.com>
2021-01-11 19:03:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -185,11 +185,11 @@ ip = process.env.NODEJS_SERVICE_IP || "0.0.0.0" || "127.0.0.1";
 //server.listen(port, ip);
 
 server.listening(port, ip); // function() {
-console.log('Server started at ' + date + ' & listening on port ' + port);
+//console.log('Server started at ' + date + ' & listening on port ' + port);
 //router.post('Server');
 
 //console.log((new Date()) + ' Server is listening on port ' + port);
-});
+//});
 
 //--------------
 
