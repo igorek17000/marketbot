e403c43759462ce1b4cdeab1bf112fb5b8d83b7c
dstlmike <dstl_mike1@hotmail.com>
2021-01-14 01:58:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -462,7 +462,7 @@ req.chunks.push(chunk.toString());
 //---});
 // -----
 
-app.use('/countdown', count, function(req, res) {
+app.get('/countdown', function(req, res) {
 //res.writeHead(200); 
 res.setHeader('Content-type', 'text/html');
 var html = fs.readFileSync(path.join(__dirname + "/views/countdown.html"));
