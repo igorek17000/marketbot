169cad0b5af7dadf21425a3635552adbe1a6e46d
dstlmike <dstl_mike1@hotmail.com>
2021-01-13 22:10:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -274,10 +274,12 @@ router.dispatch(req, res, function(err) {
 
 //console.log(app.listen);
  	return server.listen.apply(server, arguments);
-}
+
 app.listen(port, ip, function() {
 console.log('Server started at ' + date + ' & listening on port ' + port);
 });
+}
+
 //app.listen(port); //8000, ip, function() {
 //console.log('App Listening');
 //});
