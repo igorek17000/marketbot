a63f9711b18aa53435f95b2c3c373e59d5a1aec0
dstlmike <dstl_mike1@hotmail.com>
2021-01-14 01:30:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -370,7 +370,7 @@ console.log(chunk);
 
 app.get('/', function(req, res) {
 res.writeHead(200); 
-res.end("I am AlexBot.\n\For a list of commands go to\n\http://nodejs-mongo-persistent-cc.b9ad.pro-us-east-1.openshiftapps.com/login");
+res.send("I am AlexBot.\n\For a list of commands go to\n\http://nodejs-mongo-persistent-cc.b9ad.pro-us-east-1.openshiftapps.com/login");
 //res.send('Hello');
 });
 
