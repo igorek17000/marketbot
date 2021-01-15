aa89c705a62b2c445c473175fd4d3c98b4e35888
dstlmike <dstl_mike1@hotmail.com>
2021-01-14 23:04:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ req.chunks.push(chunk.toString());
 
 
 app.listen = function listen() { 	
-var server = http.createServer(app);
+var server = http.createServer(this);
  	return server.listen.apply(server, arguments);
 }
 
