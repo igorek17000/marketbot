0b076649ed8c3148ba761a993b9f72bec29eb8ef
dstlmike <dstl_mike1@hotmail.com>
2021-01-15 00:20:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -477,7 +477,7 @@ res.send(html);
 });
 
 
-app.listen(path + '/countdown', function() {
+app.listen(path, function() {
 console.log('Started ' + port); //(port, ip, function() {
 //console.log('Server started at ' + date + ' & listening on port ' + port);
 });
