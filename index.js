8d21afc4a8014a4197d3c6883a7cf0a1cb34e95a
dstlmike <dstl_mike1@hotmail.com>
2021-01-14 23:46:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -477,7 +477,7 @@ res.send(html);
 });
 
 
-app.listen('/', function() {
+app(req, res, function() {
 console.log('Started ' + port); //(port, ip, function() {
 //console.log('Server started at ' + date + ' & listening on port ' + port);
 });
