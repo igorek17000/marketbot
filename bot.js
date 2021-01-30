9832804d748ff42f93178406252f7e711ade30c4
dstlmike <dstl_mike1@hotmail.com>
2021-01-30 03:31:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,8 +18,8 @@ var dbs = mongoose.connection;
 dbs.on('error', console.log.bind(console, "connection error"));
 
 dbs.once('open', async function(callback){
-global.customerSchema = new mongoose.Schema({ name: String, email: String, pass: String, phone: String, output: String }); 
-global.Customer = mongoose.model('Customer', customerSchema, 'details'); 
+global.customerSchema = new mongoose.Schema({ name: String, password: String }); 
+global.Customer = mongoose.model('Customer', customerSchema, 'joke_bot'); 
 //global.docs = Customer.find({name}, function(err, doc)); 
 //var doc = docs.;
 //global.docs = docs;
