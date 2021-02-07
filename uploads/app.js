63daf5158245a6c6611ddb88c5752308d16c427f
dstlmike <dstl_mike1@hotmail.com>
2021-02-07 01:30:54 -0500

    Update app.js

--- uploads/app.js
+++ uploads/app.js
@@ -65,7 +65,7 @@ var storage = new GridFsStorage({
 
 var upload = multer({ storage });
 
-app.use('/uploads', imageRouter(upload));
+app.use('/', imageRouter(upload));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
