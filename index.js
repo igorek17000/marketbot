bce3c2ecb61cf007aeb9a34fb7c185f9826d0b64
dstlmike <dstl_mike1@hotmail.com>
2021-02-07 03:49:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -413,7 +413,7 @@ res.render('image', { image: 'data:image/jpeg; base64,' + base64ArrayBuffer(file
 */
 });
 
-app.use('/uploads', upp.upp);
+app.use('/uploads', upp);
 //var me = require('./uploads/server/models/image.js');
 //upp.get('/upp', (req, res) => {
 
