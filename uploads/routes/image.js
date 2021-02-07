a96f1865ffc7c925ff02d9a905635d676b0eb2d9
dstlmike <dstl_mike1@hotmail.com>
2021-02-07 01:05:52 -0500

    Update image.js

--- uploads/routes/image.js
+++ uploads/routes/image.js
@@ -10,7 +10,7 @@ module.exports = (upload) => {
 
     let gfs;
 
-gfs = new mongoose.mongo.GridFSBucket(connectt, db) {
+gfs = new mongoose.mongo.GridFSBucket { //(connectt, db) {
             bucketName: "uploads"
         }
 /*
