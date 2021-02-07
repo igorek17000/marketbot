1fdd9fa3acc9132233b600a47c2da2d3d932ec25
dstlmike <dstl_mike1@hotmail.com>
2021-02-07 16:32:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ var mods         = require('./modules/mods');
 //var countdowner  = require('./modules/countdown-list');
 //var chunks = [];
 var commandList  = require('./modules/command-list');
-var commandListSuccess  = require('./commands_success/command-list');
+//var commandListSuccess  = require('./commands_success/command-list');
 var rooms        = require('./modules/rooms');
 
 //commands with custom actions
