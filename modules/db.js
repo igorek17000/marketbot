2114e4761616ec521e5e36382710725a91901071
dstlmike <dstl_mike1@hotmail.com>
2021-02-07 15:22:45 -0500

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -1,6 +1,10 @@
 //
 var mongoDB     = require('mongodb').MongoClient;
 var db = require('mongodb').Db
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema; 
+var Image = require('uploads/models/image's);
+var ImageSchema = Image.Image;
 var bot = require('../bot.js');
 //var connection_string = 'mongodb://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
@@ -32,6 +36,12 @@ if(process.env.MONGODB_PASSWORD){
   process.env.MONGODB_DATABASE;
 }
 
+exports.connect = function connect(callback){
+  mongoDB.connect(connection_string, function(err, db) {
+    if(err) throw err;
+    callback(db);
+  });
+}
 
 function connect(callback){
   mongoDB.connect(connection_string, function(err, db) {
