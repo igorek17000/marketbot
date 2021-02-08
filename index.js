a2d7754449fcb4c185583876da00310e164a06d2
dstlmike <dstl_mike1@hotmail.com>
2021-02-08 11:40:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -257,8 +257,8 @@ app.use(bodyParser.urlencoded({
    extended: true
 }));
 
-app.set('view engine', 'ejs');
-app.engine('jade', require('jade').__express);
+app.set('view engine', 'pug');
+app.set('views', './views' || 'views', './uploads/views'); //require('jade').__express);
 //upp.engine('ejs', require('ejs').renderFile);
 
 
