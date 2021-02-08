b5847c35063d35080dcff73114817b4022f47910
dstlmike <dstl_mike1@hotmail.com>
2021-02-08 12:52:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 var app = express();
-var upp = require('./uploads//server/app.js'); //express();
+var upp = require('./uploads/server/app.js'); //express();
 var uppp = express(); //require('./uploads/routes/image');
 var mongoose = require('mongoose');
 mongoose.connect(connection_string, { useNewUrlParser: true, useUnifiedTopology: true });
