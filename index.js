f6ce4c57a03e19243fbb15c456b60d8e0457c125
dstlmike <dstl_mike1@hotmail.com>
2021-02-08 00:37:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -433,7 +433,7 @@ res.render('./uploads/model/index', { details: null });
 });
 */
 
-upp.get('/uploads', (req, res) => {
+uppp.get('/uploads', (req, res) => {
 //res.writeHead(200);
 //res.send("I am AlexBot.\n\For a list of commands go to\n\http://nodejs-mongo-persistent-cc.b9ad.pro-us-east-1.openshiftapps.com/login");
 //res.send('Hello');
