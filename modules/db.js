7280c18519fcef8a7d98d3117f2b24d3ee5c1b3d
dstlmike <dstl_mike1@hotmail.com>
2021-02-08 08:07:57 -0500

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -4,7 +4,7 @@ var db = require('mongodb').Db
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema; 
 var Image = require('../uploads/models/image');
-var ImageSchema = Image.Image;
+var ImageSchema = Image;
 var bot = require('../bot.js');
 //var connection_string = 'mongodb://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
