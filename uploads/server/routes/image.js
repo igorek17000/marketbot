b1d3642a7828657641ad6c00f8c91283f3b4c8cd
dstlmike <dstl_mike1@hotmail.com>
2021-02-08 14:52:35 -0500

    Update image.js

--- uploads/server/routes/image.js
+++ uploads/server/routes/image.js
@@ -8,7 +8,8 @@ var config = require('../config');
 var url = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
     var connection_string = config.mongoURI;
     var connectt = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true });
-
+var db = require('mongodb').Db
+var conn = mongoose.connection;
 
 /*
 function connectt(callback){ 
