63e8f915ad01a6e16daeb22a9aadce6d5ad955b7
dstlmike <dstl_mike1@hotmail.com>
2021-02-08 13:57:49 -0500

    Update image.js

--- uploads/server/routes/image.js
+++ uploads/server/routes/image.js
@@ -5,7 +5,7 @@ var Grid = require('gridfs-stream');
 Grid.mongo = mongoose.mongo;
 var Image = require('../models/image');
 var config = require('../config');
-var url = config.mongoURI;
+var url = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
     var connection_string = config.mongoURI;
     var connectt = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -20,7 +20,8 @@ callback(db);
 */
 
 module.exports = (upload) => {
-    var url = config.mongoURI;
+    var url = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
     var connection_string = config.mongoURI;
     var connectt = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -42,7 +43,7 @@ gfs = new mongoose.mongo.GridFSBucket({
 
 connectt.once('open', function () {
 
-  var gfs = Grid.mongo(connectt.db, {bucketName: 'uploads'
+  var gfs = Grid.mongo(connectt.db, { bucketName: 'uploads'
 });
 
  
