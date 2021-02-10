a12682ab38f228c07381ea6a2be2ff8524d277d7
dstlmike <dstl_mike1@hotmail.com>
2021-02-10 01:31:22 -0500

    Update homemodel.js

--- routes/homemodel.js
+++ routes/homemodel.js
@@ -123,7 +123,7 @@ router.get('/upp', function(req, res){
 		// display image from arrayBuffer stored in database
                // res.send(url.id);
       		//res.render("image.ejs", {image: 'data:image/jpeg;base64,' + base64ArrayBuffer(docs['data'])});
-res.render("image.ejs", {image: 'data:image/' + docs['contentType'] + ';base64,' + docs['data'].toString('base64')}); //base64ArrayBuffer(docs['data'])}); //<%=image.img.contentType%>;base64, 
+res.render("image.ejs", {image: 'data:image/' + docs['contentType'] + ';base64,' + docs['data']}); //base64ArrayBuffer(docs['data'])}); //<%=image.img.contentType%>;base64, 
 
                      //<%=image.img.data.toString('base64')%>"> 
 
