f148f49d47b0d3d59727bc0a94421fee98689e2b
dstlmike <dstl_mike1@hotmail.com>
2021-02-10 07:44:57 -0500

    Update UploadPage.tsx

--- uploads/client/src/components/UploadPage.tsx
+++ uploads/client/src/components/UploadPage.tsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import './uploads/client/src/style.scss';
+import './style.scss';
 import axios from 'axios';
 import Upload from '../assets/images/upload.png';
 
