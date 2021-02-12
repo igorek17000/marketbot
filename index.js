b846a48934e0d6443906988ef6cde1891637e495
dstlmike <dstl_mike1@hotmail.com>
2021-02-12 00:00:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -217,8 +217,8 @@ get: test
 server = http.createServer(function(req, res) {
   req.chunks = [];
   res.chunks = [];
-  var url = req.url;
-  url.id = url.split("/")[2];
+//  var url = req.url;
+ // url.id = url.split("/")[2];
   req.on('data', function (chunk) {
     req.chunks.push(chunk.toString());
 
@@ -231,9 +231,9 @@ server = http.createServer(function(req, res) {
 
 
 
-if(req.url === '/countdown' || req.url === '/login' || req.url === '/details' || req.url === '/detail' || req.url === '/jokes' || req.url === '/amaral' || req.url === '/images' || req.url === '/images/:id' || req.url === '/me' || req.url === '/upp' || req.url === '/delete/:id' || req.url === '/recent' || req.url === '/multiple' || req.url === '/files' || req.url === '/file/:filename' || req.url === '/image/:filename' || req.url === '/file/del/:id' || req.url === '/uploads/' || req.url === '/upp/') {
- var url = req.url;
- url.id = url.split('/')[2]; 
+if(req.url === '/countdown' || req.url === '/login' || req.url === '/details' || req.url === '/detail' || req.url === '/jokes' || req.url === '/amaral' || req.url === '/images' || req.url === '/images/:id' || req.url === '/me' || req.url === '/upp' || req.url === '/delete/:id' || req.url === '/recent' || req.url === '/multiple' || req.url === '/files' || req.url === '/file/:filename' || req.url === '/image/:filename' || req.url === '/file/del/:id' || req.url === '/uploads/' || req.url === '/upp/' || req.url === '/uploads') {
+// var url = req.url;
+ //url.id = url.split('/')[2]; 
 
  app(req, res);
 
