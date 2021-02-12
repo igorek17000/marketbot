a629aac8cb13d6cf38704c3c89698060436cecda
dstlmike <dstl_mike1@hotmail.com>
2021-02-12 02:07:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ bucketName: "uploads"
 
 //app.set('views', path.join(__dirname, 'uploads/server/views'));
 //upp.set('view engine', 'jade');
-var origin = { origin: '*' };
-app.use(cors(origin)); 
+//var origin = { origin: '*' };
+app.options('*', cors()); 
 /* {
     origin: '*'
 }));
