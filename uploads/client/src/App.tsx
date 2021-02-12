d8e565aac872eb90d93b7712ec0bf1c1e20d2b18
dstlmike <dstl_mike1@hotmail.com>
2021-02-11 23:30:56 -0500

    Update App.tsx

--- uploads/client/src/App.tsx
+++ uploads/client/src/App.tsx
@@ -35,7 +35,7 @@ class App extends PureComponent<{}> {
           <Switch>
             <Route exact path='/uploads' component={UploadPage} />
             <Route exact path='/list' component={ListPage} />
-            <Redirect from='/uploads' to='/uploads' />
+            <Redirect from='/**' to='/uploads' />
           </Switch>
         </div>
       </div>
