6960c97c66fa17cb4619cde2a45a23def9d78f40
dstlmike <dstl_mike1@hotmail.com>
2021-02-17 22:03:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -235,14 +235,14 @@ server = http.createServer(function(req, res) {
 
 
 
-if(req.url === '/' || req.url === '/init' || req.url === '/commands' || req.url === '/bot/:botRoom') {
-//if(req.url === '/' || req.url === '/countdown' || req.url === '/login' || req.url === '/details' || req.url === '/detail' || req.url === '/jokes' || req.url === '/amaral' || req.url === '/images' || req.url === '/images/:id' || req.url === '/me' || req.url === '/upp' || req.url === '/delete/:id' || req.url === '/recent' || req.url === '/multiple' || req.url === '/files' || req.url === '/file/:filename' || req.url === '/image/:filename' || req.url === '/file/del/:id' || req.url === '/uploads/' || req.url === '/upp/' || req.url === '/uploads') {
+//if(req.url === '/' || req.url === '/init' || req.url === '/commands' || req.url === '/bot/:botRoom') {
+if(req.url === '/' || req.url === '/countdown' || req.url === '/login' || req.url === '/details' || req.url === '/detail' || req.url === '/jokes' || req.url === '/amaral' || req.url === '/images' || req.url === '/images/:id' || req.url === '/me' || req.url === '/upp' || req.url === '/delete/:id' || req.url === '/recent' || req.url === '/multiple' || req.url === '/files' || req.url === '/file/:filename' || req.url === '/image/:filename' || req.url === '/file/del/:id' || req.url === '/uploads/' || req.url === '/upp/' || req.url === '/uploads') {
 // var url = req.url;
  //url.id = url.split('/')[2]; 
 
- //app(req, res);
+ app(req, res);
 
- // } else {
+  } else {
 //if(req.url === '/' || req.url === '/init' || req.url === '/bot/' || req.url === '/' || req.url === '/' || req.url === '/' || 
 router.dispatch(req, res, function(err) {
     res.writeHead(err.status, {"Content-Type": "text/plain"});
