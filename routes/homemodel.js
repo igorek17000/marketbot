07e47edb2152c5325a828438056fc3b2ef1b91ce
dstlmike <dstl_mike1@hotmail.com>
2021-03-13 00:53:57 -0500

    Update homemodel.js

--- routes/homemodel.js
+++ routes/homemodel.js
@@ -39,7 +39,7 @@ var ImageSchema = mongoose.Schema({
 
 var Image = module.exports = mongoose.model('uploads', ImageSchema);
 
-router.getImages = function(images, callback) {
+router.getImages = function(image, callback) {
 
     Image.find({}, callback);
 
@@ -113,7 +113,7 @@ res.render('image.ejs', {image: docs}); //docs['contentType'] + ';' + docs['data
 router.get('/me', function(req, res, next){
 var images = {};
 var id = '60235c64d1e4d5001aa0760c';
-	router.getImages(images, (err, items) => {
+	router.getImages(image, (err, images) => {
 
 		if (err) {
 
