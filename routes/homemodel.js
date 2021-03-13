398120d58d208879c88be1b0b2bae72a009e610b
dstlmike <dstl_mike1@hotmail.com>
2021-03-12 23:47:33 -0500

    Update homemodel.js

--- routes/homemodel.js
+++ routes/homemodel.js
@@ -124,7 +124,7 @@ var id = '60235c64d1e4d5001aa0760c';
 		// display image from arrayBuffer stored in database
                // res.send(url.id);
       		//res.render("image.ejs", {image: 'data:' + docs['contentType'] + ';base64,' + base64ArrayBuffer(docs['data'])});
-res.render("images.ejs", {images: items}); //"data:" + docs['contentType'] + ";base64," + base64ArrayBuffer(docs['data'])}); //<%=image.img.contentType%>;base64, 
+res.render("img.ejs", {images: items}); //"data:" + docs['contentType'] + ";base64," + base64ArrayBuffer(docs['data'])}); //<%=image.img.contentType%>;base64, 
 
                      //<%=image.img.data.toString('base64')%>"> 
 
