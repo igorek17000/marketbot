8b1dcb498346b5facc6764c9fd2357f7c9651dd4
dstlmike <dstl_mike1@hotmail.com>
2021-03-22 23:57:41 -0400

    Update image.js

--- uploads/server/routes/image.js
+++ uploads/server/routes/image.js
@@ -81,7 +81,7 @@ module.exports = (upload) => {
                 .then(images => {
                     res.status(200).json({
                         success: true,
-                        images[name],
+                        images.name,
                     });
                 })
                 .catch(err => res.status(500).json(err));
