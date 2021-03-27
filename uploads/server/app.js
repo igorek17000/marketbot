d681d2c69e1c3f99a0ef50ec553173ae36b5ca23
dstlmike <dstl_mike1@hotmail.com>
2021-03-27 00:38:11 -0400

    Update app.js

--- uploads/server/app.js
+++ uploads/server/app.js
@@ -20,7 +20,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); //'jade' || 'ejs');
+app.set('view engine', 'ejs' || 'jade'); //'jade' || 'ejs');
 //app.use('view engine', 'ejs');
 app.use(cors({
     origin: '*',
