da5735f71abe9faa1b58e570a9eb1bae9e1716ab
dstlmike <dstl_mike1@hotmail.com>
2021-04-09 00:21:41 -0400

    Update app.js

--- uploads/server/app.js
+++ uploads/server/app.js
@@ -38,12 +38,12 @@ mongoose.Promise = require('bluebird');
 var url = config.mongoURI;
 var connectt = mongoose.connection; //connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 connectt;
-/*
+
 // connect to the database
 connecttt.then(() => {
   console.log('Connected to database: GridApp');
 }, (err) => console.log(err));
-*/
+
 /*
     GridFs Configuration
 */
