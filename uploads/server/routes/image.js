dbac9131a399e3d8562b11152555279f228e2e6b
dstlmike <dstl_mike1@hotmail.com>
2021-04-09 00:56:49 -0400

    Update image.js

--- uploads/server/routes/image.js
+++ uploads/server/routes/image.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var imageRouter = express.Router();
 var mongoose = require('mongoose');
-var Grid = require('gridfs-stream');
-Grid.mongo = mongoose.mongo;
 var Image1 = require('../models/image');
 var config = require('../config');
-var url = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+//var url = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
     var connection_string = config.mongoURI;
    // mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 //var db = require('mongodb').Db;
@@ -24,7 +22,9 @@ callback(db);
 //
 
 module.exports = (upload) => {
-    const url = config.mongoURI;
+   // const url = config.mongoURI;
+var url = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+ 
     const connect = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
     let gfs;
