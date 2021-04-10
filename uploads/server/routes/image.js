d0ac87105a82c6b4a49bab9e024e8e452b44e895
dstlmike <dstl_mike1@hotmail.com>
2021-04-10 03:37:27 -0400

    Update image.js

--- uploads/server/routes/image.js
+++ uploads/server/routes/image.js
@@ -25,7 +25,7 @@ module.exports = (upload) => {
    // const url = config.mongoURI;
 var url = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
  
-    const connect = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    const connect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }); // mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
     let gfs;
 
