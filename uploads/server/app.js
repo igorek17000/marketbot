7cfad567aa93d49125e1d0191d0dea5d485f7918
dstlmike <dstl_mike1@hotmail.com>
2021-04-10 00:43:06 -0400

    Update app.js

--- uploads/server/app.js
+++ uploads/server/app.js
@@ -51,7 +51,7 @@ connectt.then(() => {
 
 // create storage engine
 var storage = new GridFsStorage({
-    url: 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';  //config.mongoURI,
+    url: 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',  //config.mongoURI,
     file: (req, file) => {
         return new Promise((resolve, reject) => {
             crypto.randomBytes(16, (err, buf) => {
