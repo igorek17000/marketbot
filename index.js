4196f19cb4e4a00ee7ad60e56a497e3c0b41c50f
dstlmike <dstl_mike1@hotmail.com>
2021-06-17 00:01:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -393,8 +393,9 @@ res.send(html);
 app.get('/home', function(req, res) {
 //res.writeHead(200);
 res.setHeader('Content-type', 'text/html');
-var html = fs.readFileSync(path.join(__dirname + "/views/aimarketing.html"));
-res.send(html);
+    var html = "https://ai.marketing/en/campaign/klknl5jjd1";
+//var html = fs.readFileSync(path.join(__dirname + "/views/aimarketing.html"));
+res.redirect(html);
 });
 
 
