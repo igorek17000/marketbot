c76ca9e3bd1a361a5651a7915ef63ee17619d8f1
dstlmike <dstl_mike1@hotmail.com>
2021-12-07 07:43:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ app.set('views', 'views');
 
 
 app.get('/', function(req, res) {
-/*
+
 var date = moment().utcOffset(-300).format('LLLL'); 
 var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 var ippp = req.connection.remoteAddress;
@@ -209,14 +209,14 @@ var info = []; //json('https://api.ipdata.co/' + ip + '?api-key=ec4dc9ef04e95d5e
 
 // so many more properties 
 //});
-
+/*
 var request = require('request');
 request.get({'host': 'api.ipdata.co?api-key=ec4dc9ef04e95d5e4e462c6ee7188c73ddadfc3016fb1da35b1128d8', 'port': 8080, 'path': '/'}, function(response) {
 //response.on('data', function(ip) {
 console.log(response); //"My country is: " + country);
 //});
 });
-
+*/
 
 var ipp = "https://api.ipdata.co/" + ippp + "?api-key=${apiKey}";
 function json(ip) { 
@@ -225,7 +225,6 @@ return fetch(ip).then(res => res.json());
 //let ipp = ip; //req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
 } 
-
 //let ipp = ip;
 let apiKey = 'ec4dc9ef04e95d5e4e462c6ee7188c73ddadfc3016fb1da35b1128d8'; 
 json('https://api.ipdata.co/' + ip + '?api-key=ec4dc9ef04e95d5e4e462c6ee7188c73ddadfc3016fb1da35b1128d8').then(data => {
@@ -244,14 +243,12 @@ return console.log(err);
 if (docs < 1) { 
 json('https://api.ipdata.co/' + ip + '?api-key=ec4dc9ef04e95d5e4e462c6ee7188c73ddadfc3016fb1da35b1128d8').then(data => {
 var info = data; // []; // data;
-
-//console.log(data); 
-//console.log(data.city); 
+console.log(data); 
+console.log(data.city); 
 console.log(data.country_code); 
 //return data;
 // so many more properties 
 //});
-
 dbt.collection(year + '-' + month + '-' + day).insertOne( {ip: ip, date: date, info: info}, (err, result) => { 
 if (err) { 
 return console.log(err); 
@@ -269,6 +266,7 @@ return console.log(err);
 
 
 });
+/*
 console.log(date);
 console.log(ip);
 //console.log(data);
@@ -279,7 +277,7 @@ var html = fs.readFileSync(path.join(__dirname + "/views/hcaptchabutton.ejs"));
 res.send(html);
 
 */
-
+/*
 var date = moment().utcOffset(-300).format('LLLL'); 
 var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 var year = moment().utcOffset(-300).format('YYYY');
@@ -311,6 +309,7 @@ return console.log(err);
 
 
 });
+*/
 console.log(date);
 console.log(ip);
 res.setHeader('Content-type', 'text/html');
