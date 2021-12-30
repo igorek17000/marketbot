052fedb89b30c41bc599db3eee54afae3b12dab5
dstlmike <dstl_mike1@hotmail.com>
2021-12-30 00:27:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,8 @@ app(req, res);
 
 port = Number(process.env.NODEJS_SERVICE_PORT || process.env.PORT || 8080 || 3002);
 ip = process.env.NODEJS_SERVICE_IP || "0.0.0.0" || "127.0.0.1";
-
+GMAIL_USER = process.env.GMAIL_USER;
+GMAIL_PASSWORD = process.env.GMAIL_PASSWORD;
 server.listen(port, ip, function() {
 var date = moment().utcOffset(-240).format('LLLL');
 console.log('Server started at ' + date + ' & listening on port ' + port);
