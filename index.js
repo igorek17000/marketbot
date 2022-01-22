ee4c3cbd3e4e2bd90db5c4787d17c54eab4280ac
dstlmike <dstl_mike1@hotmail.com>
2022-01-22 04:42:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -579,7 +579,7 @@ res.send(html);
 });
 
 
-app.get('https://ai-marketing-canada.b9ad.pro-us-east-1.openshiftapps.com', (req, res) => {
+app.get('/signin', (req, res) => {
 console.log('Redirected from ' + req.url);
 
 res.setHeader('Content-type', 'text/html');
