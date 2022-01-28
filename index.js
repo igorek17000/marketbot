4f42a229f3045de46cc3b0564950d973fe26d08a
dstlmike <dstl_mike1@hotmail.com>
2022-01-28 17:52:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ GMAIL_PASSWORD = process.env.GMAIL_PASSWORD;
 
 
 //var express = require("express");
-var bodyParser = require('body-parser");
+var bodyParser = require('body-parser');
 var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 //var app = express();
 var upp = require('./uploads/server/app.js'); //express();
