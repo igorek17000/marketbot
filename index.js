9f77050a86961b43a404a1d56257649c656d904e
dstlmike <dstl_mike1@hotmail.com>
2022-01-28 17:24:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ console.log('Server started at ' + date + ' & listening on port ' + port);
 //--------------
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
    extended: true
 }));
