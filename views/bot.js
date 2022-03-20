eac9e99a1015b6d236b8777a74eb47a81fbb6054
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-03-19 23:57:38 -0400

    Create bot.js

---
+++ views/bot.js
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-199797979-2"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-199797979-2');
+</script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width">
+<link rel="preload" href="/animation.c88f848.svg" as="image">
+<link rel="preload" href="/logo.a1a2f16.svg" as="image">
+<title>MarketBot</title>
+<link data-n-head="1" rel="icon" type="image/x-icon" href="/favicon-32x32-1.png">
+<meta name="description" content="Wait what, An Artificial Intelligence Robot. The digital world is inheriting the use of Bots. An Autonomous computer program on the internet or another network that can interact with systems or users. Put the internet to work for you. Hire a Bot today">
+<link rel="canonical" href="https://www.marketbotai.com/info">
+<link rel="shortcut icon" href="/favicon-32x32-1.png" type="image/x-icon">
+
+<style>
+@media screen and (max-width: 500px) {
+html {
+font-size: 16px !important;
+}
+#css-top-div {
+min-height: 70% !important;
+}
+#css-menu {
+top: 1%;
+width: 100%;
+}
+#css-bottom-div { 
+font-size: 10px !important;
+}
+#parent a {
+font-size: 16px !important;
+line-height: 1rem;
+}
+footer {
+font-size: 8px;
+}
+}
+@media screen and (min-width: 961px) {
+html {
+font-size: 25px !important;
+}
+}
+html {
+margin: 0px 0px;
+background-color: black;
+color: white;
+position: relative;
+font-family: 'IBM-Plex-Regular', serif;
+}
+body {
+margin: 0px 0px;
+}
+img {
+width: 90%;
+height: auto;
+margin: auto;
+display: block;
+}
+#css-svg {
+width: 40%;
+display: block;
+margin-left: 5%;
+margin-top: 10%;
+}
+h1 {
+margin-top: 22%;
+margin-bottom: 5%;
+}
+#css-top-div {
+background: url('/logo.a1a2f16.svg') 10% 5% no-repeat, linear-gradient(to right, #003665, #00a3b6);
+background-size: 40% auto, 100% auto;
+position: relative;
+text-align: center;
+width: 100%;
+display: inline-block;
+margin: 0px 0px;
+}
+#css-bottom-div {
+position: relative;
+display: inline-block;
+width: 100%;
+}
+#css-bottom {
+position: relative;
+bottom: 1%;
+}
+#css-padding {
+margin: 5%;
+}
+#css-align {
+margin-top: 30%;
+}
+footer {
+text-align: center;
+}
+#css-bottom a {
+display: inline-block;
+text-align: center; 
+vertical-align: middle;
+border: 1px solid transparent;
+border-radius: 5%;
+color: #000000;
+padding: 1%;
+background-color: #007bff;
+font-weight: bolder;
+cursor: pointer;
+text-decoration: none;
+}
+#css-menu {
+top: 1%;
+width: 100%;
+}
+#parent {
+font-size: 1.5rem;
+display: block;
+position: relative;
+text-align: center;
+font-weight: bolder;
+float: left;
+line-height: 3rem;
+background-color: inherit;
+border: none;
+}
+#parent a {
+color: white;
+text-align: center;
+line-height: inherit;
+padding-left: 0.3rem;
+padding-right: 0.3rem;
+text-decoration: none;
+}
+#parent:hover > ul {
+display: block;
+position: absolute;
+}
+#child {
+display: none;
+}
+#child li {
+background-color: #E4EFF7;
+line-height: inherit;
+border-bottom: #CCC 1px solid;
+border-right: #CCC 1px solid; 
+width: 100%;
+}
+#child li a {
+color: #000000;
+}
+ul {
+list-style: none;
+margin: 0px;
+padding: 0px;
+min-width: 20%;
+}
+ul ul ul { 
+left: 100%;
+top: 0px;
+margin-left: 1px;
+}
+li:hover {
+background-color: inherit;
+}
+#parent li:hover {
+background-color: #F0F0F0;
+}
+#expand {
+font-size: inherit;
+font-weight: bolder;
+float: right;
+margin-right: 1px;
+}
+</style>
+</head>
+<body>
+<div id="css-top-div">
+<ul id="css-menu">
+<li id="parent"><a href="/">Home</a></li>
+<li id="parent"><a href="https://ai.marketing/en/campaign/dwkxjvw4m6">Sign In</a></li>
+<li id="parent"><a href="#">Fund MarketBot</a>
+<ul id="child">
+<li id="parent"><a href="#">Canada <span class="expand">»</span></a>
+<ul id="child">
+<li><a href="https://refer.ndax.io/vdEA"> NDAX </a></li>
+</ul>
+</li>
+<li id="parent"><a href="#">United States <span class="expand">»</span></a>
+<ul id="child">
+<li><a href="https://www.binance.com/en/activity/referral-entry?fromActivityPage=true&ref=LIMIT_VLT3YNPT"> Binance </a></li>
+<li><a href="https://payeer.com/018427100"> Payeer </a></li>
+<li><a href="https://robinhood.com/us/en/"> Robinhood</a></li>
+</ul>
+</li>
+<li id="parent"><a href="#">International <span class="expand">»</span></a>
+<ul id="child">
+<li><a href="https://www.binance.com/en/activity/referral-entry?fromActivityPage=true&ref=LIMIT_VLT3YNPT"> Binance </a></li>
+<li><a href="https://www.gate.io/"> Gate.io</a></li>
+<li><a href="https://www.kraken.com/"> Kraken</a></li>
+</ul>
+</li>
+</ul>
+</li>
+<li id="parent"><a href="https://ai.marketing/en/campaign/xkknl8vip0">Sign Up</a></li>
+</ul>
+<h1>MarketBot</h1>
+<img width="183" height="150" src="/animation.c88f848.svg" alt="ai marketing logo">
+<div id="css-padding">Wait what, An Artificial Intelligence Robot?<br>The digital world is inheriting the use of Bots.<br>An Autonomous computer program on the internet or another network that can interact with systems or users.<br>Put the internet to work for you. Hire a Bot today<br><br></div>
+<div id="css-bottom">Find out how AI Marketing's MarketBot can change your business. <br> Click below to watch a video tutorial <br><br> <a href="https://ai.marketing/en/campaign/xkknl8vip0">Watch Video</a></div>
+</div>
+<footer>All Rights Reserved.</footer>
+</body>
+</html>
