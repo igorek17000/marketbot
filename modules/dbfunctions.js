5030c350ddb08cc45f5136e323f324ee343a42c4
Alex De Aguiar <alexdea@Alexs-MacBook-Pro.local>
2022-06-14 22:47:39 -0400

    Second

--- modules/dbfunctions.js
+++ modules/dbfunctions.js
@@ -28,13 +28,13 @@ var getAllDocuments = async function(reqUrl) {
   var year = moment().utcOffset(-240).format('YYYY');
   var month = moment().utcOffset(-240).format('MM');
   var day = moment().utcOffset(-240).format('DD');
-  var matchHash = { ip: is_ip, reqUrl: reqUrl, date: date, time: time, info: ipdata};
+  var matchHash = { ip: ip, reqUrl: reqUrl, date: date, time: time, info: ipdata};
   var repeat = { date, is_ip, time, reqUrl};
   var info = ipdata;
   reqUrl = reqUrl;
 date = date;
 var ip = ip; //ip.split(/, /)[0]; //is_ip;
-dbt.collection(db_table).find(ipp).toArray(function(err, docs) {
+dbt.collection(db_table).find(ip).toArray(function(err, docs) {
 
 if (err) {
 return console.log(err);
@@ -46,7 +46,7 @@ if (docs < 1) {
 }
   });
 } else if(docs) {
-dbt.collection(db_table2).find(ipp).toArray(function(err, docs) {
+dbt.collection(db_table2).find(ip).toArray(function(err, docs) {
   if (err) {
   return console.log(err);
 }
@@ -57,7 +57,7 @@ dbt.collection(db_table2).find(ipp).toArray(function(err, docs) {
   }
 });
   } else if(docs) {
-  dbt.collection(db_table3).find(ipp).toArray(function(err, docs) {
+  dbt.collection(db_table3).find(ip).toArray(function(err, docs) {
     if (err) {
     return console.log(err);
 }
