32bd0b178fbfed2aa19431206217089bae662431
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-06-15 01:26:55 -0400

    Update dbfunctions.js

--- modules/dbfunctions.js
+++ modules/dbfunctions.js
@@ -68,7 +68,7 @@ dbt.collection(db_table2).find({"ip": ipp}).toArray(function(err, docs) {
     }
   });
 } else if(docs) {
-  dbt.collection(db_table3).updateOne( {"ip": ipp}, {$push: {repeat}}, (err, result) => {
+  dbt.collection(db_table3).updateOne( {"ip": ipp}, {$set: {repeat}}, (err, result) => {
   if (err) {
   return console.log(err);
   }
