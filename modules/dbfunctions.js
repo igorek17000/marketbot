b367bae089e19b54a88a43ac3c2c3759d1844c8b
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-06-16 03:21:39 -0400

    Update dbfunctions.js

--- modules/dbfunctions.js
+++ modules/dbfunctions.js
@@ -28,7 +28,7 @@ var getAllDocuments = async function(ipp, reqUrl) {
   var datee = moment().utcOffset(-240).format('LL');
   var time = moment().utcOffset(-240).format('LTS');
 
-var timee = "moment().utcOffset(-240).format('LTS')";
+var timee = moment().utcOffset(-240).format('LTS');
   var year = moment().utcOffset(-240).format('YYYY');
   var month = moment().utcOffset(-240).format('MM');
   var day = moment().utcOffset(-240).format('DD');
