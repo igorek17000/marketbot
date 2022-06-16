64072c010e48d2383a140b03a7b4de2c14a3368b
Alex De Aguiar <alexdea@Alexs-MacBook-Pro.local>
2022-06-15 22:55:37 -0400

    Forth

--- routes/routes.js
+++ routes/routes.js
@@ -25,27 +25,27 @@ app.use(async function(req, res, next) {
   var ipp = ippp.split(/, /)[0];
   var ipdata = await getIpData(ipp);
   var { is_threat, is_anonymous, is_known_attacker, is_known_abuser } = ipdata.threat;
-   
+
   if (!is_threat) {
-    console.log("Blocked IP");
+    console.log("Blocked IP at " + date + " " + time + "\n" + ipdata);
     res.status(403).end("Access Denied");
     return;
   }
-   
+
     if (is_known_abuser) {
-    console.log("Blocked IP");
+    console.log("Blocked IP at " + date + " " + time + "\n" + ipdata);
     res.status(403).end("Access Denied");
     return;
   }
-   
+
     if (is_known_attacker) {
-    console.log("Blocked IP");
+    console.log("Blocked IP at " + date + " " + time + "\n" + ipdata);
     res.status(403).end("Access Denied");
     return;
   }
-   
+
   if (is_anonymous) {
-    console.log("VPN's are not allowed");
+    console.log("VPN's are not allowed at " + date + " " + time + "\n" + ipdata);
     res.status(403).end("VPN's are not allowed."); //"VPNs are not allowed");
     return;
   }
