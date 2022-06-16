513d03a14f479f3c5cd9a7cef9dc76ea2f283fa7
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-06-16 02:07:48 -0400

    Update dbfunctions.js

--- modules/dbfunctions.js
+++ modules/dbfunctions.js
@@ -72,7 +72,7 @@ dbt.collection(db_table2).find({"ip": ipp}).toArray(function(err, docs) {
     }
   });
 } else if(docs) {
-  dbt.collection(db_table3).updateOne( {"ip": ipp}, {$set: {repeat: {time}: {repeat}}, (err, result) => {
+  dbt.collection(db_table3).updateOne( {"ip": ipp}, {$set: {repeat: {time}, {repeat}}, (err, result) => {
 
   //dbt.collection(db_table3).updateOne( {"ip": ipp}, {$push: {repeat}}, (err, result) => {
   if (err) {
