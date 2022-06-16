c2b205a2697304912d0fb5c234cb378d09699672
Alex De Aguiar <alexdea@Alexs-MacBook-Pro.local>
2022-06-15 23:28:31 -0400

    Forth

--- routes/routes.js
+++ routes/routes.js
@@ -24,7 +24,7 @@ app.use(async function(req, res, next) {
   var ippp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   var ipp = ippp.split(/, /)[0];
   var ipdata = await getIpData(ipp);
-  var ipdataa = {};
+  var ipdataa = [];
   ipdataa = ipdata;
   var { is_threat, is_anonymous, is_known_attacker, is_known_abuser } = ipdata.threat;
 
