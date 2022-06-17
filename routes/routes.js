1bd87a4a030e5188da867f31ed439fb72def76b0
Alex De Aguiar <alexdea@Alexs-MacBook-Pro.local>
2022-06-16 23:00:06 -0400

    Sixth

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ var routes = express();
 var bodyParser = require('body-parser');
 var axios = require('axios');
 var ejs = require('ejs');
+var nodemailer = require('nodemailer');
 var moment = require('moment');
 var date = moment().utcOffset(-240).format('LL');
 var time = moment().utcOffset(-240).format('LTS');
@@ -31,19 +32,16 @@ app.use(async function(req, res, next) {
     res.status(403).end("Access Denied");
     return;
   }
-
     if (is_known_abuser) {
     console.log("Blocked is_known_abuser IP at " + date + " " + time + "\n" + ipdata.ip);
     res.status(403).end("Access Denied");
     return;
   }
-
     if (is_known_attacker) {
     console.log("Blocked is_known_attacker IP at " + date + " " + time + "\n" + ipdata.ip);
     res.status(403).end("Access Denied");
     return;
   }
-
   if (is_anonymous) {
     console.log("VPN's are not allowed at " + date + " " + time + "\n" + ipdata.ip);
     res.status(403).end("VPN's are not allowed.");
@@ -51,7 +49,6 @@ app.use(async function(req, res, next) {
   }
 
   //if (process.env.NODE_ENV != 'development') {
-
   if (!req.secure || req.headers.host == 'elb.b9ad.pro-us-east-1.openshiftapps.com' || req.headers.host == 'ai-marketing.b9ad.pro-us-east-1.openshiftapps.com') {
   return res.status(301).redirect('https://marketbotai.com/home');
   }
@@ -65,6 +62,7 @@ app.use(async function(req, res, next) {
   });
 
 app.get('/', async function(req, res, next) {
+  var logg = date + ' ' + time + '\n' + ip + '\n' + req.protocol + '://' + req.hostname + '\n' + req.url + '\n' + 'Location: {' + '\n' + 'City: ' + data.city + ', \n' + 'Contry: ' + data.country_name + ', \n' + 'Postal: ' + data.postal + ', \n' + '},' + '\n' + 'Asn: {' + '\n' + 'Name: ' + data.asn.name + ', \n' + 'Domain: ' + data.asn.domain + '\n' + '}';
   var ippp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   var ipp = ippp.split(/, /)[0];
   var ipdata = await getIpData(ipp);
@@ -74,8 +72,23 @@ app.get('/', async function(req, res, next) {
 
 getAllDocuments(ipp, reqUrl);
 
-  console.log(ipdata);
+  console.log(logg);
   res.render('menu2.ejs');
+});
+
+app.get('/home', async function(req, res, next) {
+  var logg = date + ' ' + time + '\n' + ip + '\n' + req.protocol + '://' + req.hostname + '\n' + req.url + '\n' + 'Location: {' + '\n' + 'City: ' + data.city + ', \n' + 'Contry: ' + data.country_name + ', \n' + 'Postal: ' + data.postal + ', \n' + '},' + '\n' + 'Asn: {' + '\n' + 'Name: ' + data.asn.name + ', \n' + 'Domain: ' + data.asn.domain + '\n' + '}';
+  var ippp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var ipp = ippp.split(/, /)[0];
+  var ipdata = await getIpData(ipp);
+  var { ip } = ipdata;
+  var reqUrl = req.path;
+  var { is_threat, is_anonymous } = ipdata.threat;
+
+getAllDocuments(ipp, reqUrl);
+
+  console.log(logg);
+  res.render('menu.ejs');
 });
 
 
