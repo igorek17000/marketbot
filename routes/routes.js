b06fe7dfaa430597f6c8766345223b6fe90f9d48
Alex De Aguiar <alexdea@Alexs-MacBook-Pro.local>
2022-06-16 23:11:25 -0400

    Sixth

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ var routes = express();
 var bodyParser = require('body-parser');
 var axios = require('axios');
 var ejs = require('ejs');
+var path = require('path');
 var nodemailer = require('nodemailer');
 var moment = require('moment');
 var date = moment().utcOffset(-240).format('LL');
@@ -15,6 +16,9 @@ var day = moment().utcOffset(-240).format('DD');
 var getIpData = require('../modules/ipdata.js');
 getAllDocuments = require('../modules/dbfunctions.js')
 app.set('trust proxy', true);
+app.use(express.static(path.join(__dirname + '/partials')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
    extended: true
@@ -62,7 +66,7 @@ app.use(async function(req, res, next) {
   });
 
 app.get('/', async function(req, res, next) {
-  var logg = date + ' ' + time + '\n' + ipp + '\n' + req.protocol + '://' + req.hostname + '\n' + req.url + '\n' + 'Location: {' + '\n' + 'City: ' + ipdata.city + ', \n' + 'Contry: ' + ipdata.country_name + ', \n' + 'Postal: ' + ipdata.postal + ', \n' + '},' + '\n' + 'Asn: {' + '\n' + 'Name: ' + ipdata.asn.name + ', \n' + 'Domain: ' + ipdata.asn.domain + '\n' + '}';
+  var logg = date + ' ' + time + '\n' + ipdata.ip + '\n' + req.protocol + '://' + req.hostname + '\n' + req.url + '\n' + 'Location: {' + '\n' + 'City: ' + ipdata.city + ', \n' + 'Contry: ' + ipdata.country_name + ', \n' + 'Postal: ' + ipdata.postal + ', \n' + '},' + '\n' + 'Asn: {' + '\n' + 'Name: ' + ipdata.asn.name + ', \n' + 'Domain: ' + ipdata.asn.domain + '\n' + '}';
   var ippp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   var ipp = ippp.split(/, /)[0];
   var ipdata = await getIpData(ipp);
@@ -77,7 +81,7 @@ getAllDocuments(ipp, reqUrl);
 });
 
 app.get('/home', async function(req, res, next) {
-  var logg = date + ' ' + time + '\n' + ipp + '\n' + req.protocol + '://' + req.hostname + '\n' + req.url + '\n' + 'Location: {' + '\n' + 'City: ' + ipdata.city + ', \n' + 'Contry: ' + ipdata.country_name + ', \n' + 'Postal: ' + ipdata.postal + ', \n' + '},' + '\n' + 'Asn: {' + '\n' + 'Name: ' + ipdata.asn.name + ', \n' + 'Domain: ' + ipdata.asn.domain + '\n' + '}';
+  var logg = date + ' ' + time + '\n' + ipdata.ip + '\n' + req.protocol + '://' + req.hostname + '\n' + req.url + '\n' + 'Location: {' + '\n' + 'City: ' + ipdata.city + ', \n' + 'Contry: ' + ipdata.country_name + ', \n' + 'Postal: ' + ipdata.postal + ', \n' + '},' + '\n' + 'Asn: {' + '\n' + 'Name: ' + ipdata.asn.name + ', \n' + 'Domain: ' + ipdata.asn.domain + '\n' + '}';
   var ippp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   var ipp = ippp.split(/, /)[0];
   var ipdata = await getIpData(ipp);
