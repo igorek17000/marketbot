b69496dc15cfe41754e51389c5dd6f8240d90409
Alex De Aguiar <alexdea@Alexs-MacBook-Pro.local>
2022-06-20 17:44:59 -0400

    2.03

--- modules/ipdata.js
+++ modules/ipdata.js
@@ -2,10 +2,10 @@ var express = require('express');
 var axios = require('axios');
 var app = express();
 var getIpData = async (ipp) => {
-var ipp = ipp;
+let ipp = ipp;
   var IpDataKey = 'ec4dc9ef04e95d5e4e462c6ee7188c73ddadfc3016fb1da35b1128d8';
   try {
-    var ipp = ipp;
+   let ipp = ipp;
     var IpDataKey = 'ec4dc9ef04e95d5e4e462c6ee7188c73ddadfc3016fb1da35b1128d8';
   var res = await axios.get('https://api.ipdata.co/' + ipp + '?api-key=' + IpDataKey);
 }
