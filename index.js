9fcba26c40f9d1faa3aa58a3eb74f964d040d5b8
Alex De Aguiar <alexdea@Alexs-MacBook-Pro.local>
2022-06-20 23:13:53 -0400

    2.04

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ var bodyParser = require('body-parser');
 var axios = require('axios');
 var getIpData = require('./modules/ipdata.js');
 var routes = require('./routes/routes.js');
+var moment = require('moment');
+var date = moment().utcOffset(-240).format('LLLL');
 var port = Number(process.env.NODEJS_SERVICE_PORT || process.env.PORT || 8080 || 3002);
-//var ip = process.env.NODEJS_SERVICE_IP || "0.0.0.0" || "127.0.0.1";
 app.enable('trust proxy');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
    extended: true
 }));
 
-   //var ip = '24.114.79.251';
-   //var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
-app.use(routes);
+   app.use(routes);
 
 app.listen(port, function(error) {
   if(error) {
     console.log(error);
   }
-  console.log('Server running on Port: ' + port);
+  console.log('Server started at ' + date + ' and running on Port: ' + port);
 });
